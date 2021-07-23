b010fb1d99f9e3efa0fda9716ae8f559040d7835
flmoro <fernandoluizmoro@gmail.com>
2021-07-23 09:16:51 -0300

    feat(instance): add the InstanceIdentifier class

---
+++ ipipeline/utils/instance.py
@@ -0,0 +1,28 @@
+import re
+from typing import List
+
+from ..exceptions import InstanceError
+
+
+class InstanceIdentifier:
+    def __init__(self, id_: str, tags: List[str] = []) -> None:
+        self._id = self._check_id(id_)
+        self._tags = tags
+
+    @property
+    def id(self) -> str:
+        return self._id
+
+    @property
+    def tags(self) -> List[str]:
+        return self._tags
+
+    def _check_id(self, id_: str) -> str:
+        if re.fullmatch(r'[\w-]+', id_):
+            return id_
+        else:
+            raise InstanceError(
+                'id_ does not match the pattern '
+                '(only letters, digits, underscore and/or dash)',
+                f'id_ == {id_}'
+            )
