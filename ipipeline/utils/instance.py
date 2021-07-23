e22bad3399b5914d17851029f9010536c3a2c6c1
flmoro <fernandoluizmoro@gmail.com>
2021-07-23 10:20:10 -0300

    feat(instance): add the create_instance_repr func

--- ipipeline/utils/instance.py
+++ ipipeline/utils/instance.py
@@ -1,4 +1,5 @@
 import re
+from inspect import signature
 from typing import List
 
 from ..exceptions import InstanceError
@@ -26,3 +27,26 @@ class InstanceIdentifier:
                 '(only letters, digits, underscore and/or dash)',
                 f'id_ == {id_}'
             )
+
+
+def create_instance_repr(instance: object) -> str:
+    param_reprs = []
+
+    for param in signature(instance.__init__).parameters.values():
+        value = None
+
+        for attr in [
+            f'_{param.name}', param.name, 
+            f'_{param.name[:-1]}', param.name[:-1]
+        ]:
+            if attr in instance.__dict__:
+                value = getattr(instance, attr)
+
+                if isinstance(value, str):
+                    value = f'\'{value}\''
+
+                break
+
+        param_reprs.append(f'{param.name}={value}')
+
+    return f'{instance.__class__.__name__}({", ".join(param_reprs)})'
