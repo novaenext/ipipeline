5d6a0ab2fc986a624fd38a8772a82c39e956cd02
flmoro <fernandoluizmoro@gmail.com>
2021-07-26 08:43:54 -0300

    test(structure): update the name of the test methods

--- tests/structure/test_conn.py
+++ tests/structure/test_conn.py
@@ -5,16 +5,16 @@ from ipipeline.structure.conn import BaseConn, Conn
 
 
 class TestBaseConn(TestCase):
-    def test_valid_instance(self) -> None:
-        base_conn = BaseConn('c1', 'n1', 'n2', tags=['math'])
+    def test_init_valid_args(self) -> None:
+        base_conn = BaseConn('c1', 'n1', 'n2', tags=['data'])
 
         self.assertEqual(base_conn.id, 'c1')
         self.assertEqual(base_conn.src_id, 'n1')
         self.assertEqual(base_conn.dst_id, 'n2')
         self.assertEqual(base_conn.weight, 0)
-        self.assertListEqual(base_conn.tags, ['math'])
+        self.assertListEqual(base_conn.tags, ['data'])
 
-    def test_invalid_instance(self) -> None:
+    def test_init_invalid_args(self) -> None:
         with self.assertRaisesRegex(
             InstanceError, 
             r'id_ does not match the pattern \(only letters, digits, '
@@ -22,7 +22,7 @@ class TestBaseConn(TestCase):
         ):
             _ = BaseConn('c1,', 'n1', 'n2')
 
-    def test_instance_repr(self) -> None:
+    def test_repr(self) -> None:
         base_conn = BaseConn('c1', 'n1', 'n2')
         instance_repr = base_conn.__repr__()
 
@@ -39,7 +39,7 @@ class TestBaseConn(TestCase):
 
 
 class TestConn(TestCase):
-    def test_base_class(self) -> None:
+    def test_new_valid_args(self) -> None:
         conn = Conn('c1', 'n1', 'n2')
         
         self.assertIsInstance(conn, BaseConn)
--- tests/structure/test_node.py
+++ tests/structure/test_node.py
@@ -4,28 +4,28 @@ from ipipeline.exceptions import InstanceError
 from ipipeline.structure.node import BaseNode, Node
 
 
-def mock_sum(param1: int, param2: int = 0) -> int:
-    return sum([param1, param2])
+def mock_func(param1: int) -> int:
+    return param1
 
 
 class TestBaseNode(TestCase):
-    def test_valid_instance(self) -> None:
+    def test_init_valid_args(self) -> None:
         base_node = BaseNode(
             'n1', 
-            mock_sum, 
-            inputs={'param1': 7, 'param2': 3}, 
-            outputs=['sum'],
-            tags=['math']
+            mock_func, 
+            inputs={'param1': 7}, 
+            outputs=['return1'],
+            tags=['data']
         )
 
         self.assertEqual(base_node.id, 'n1')
-        self.assertEqual(base_node.func, mock_sum)
-        self.assertDictEqual(base_node.inputs, {'param1': 7, 'param2': 3})
-        self.assertListEqual(base_node.outputs, ['sum'])
+        self.assertEqual(base_node.func, mock_func)
+        self.assertDictEqual(base_node.inputs, {'param1': 7})
+        self.assertListEqual(base_node.outputs, ['return1'])
         self.assertListEqual(base_node.props, [])
-        self.assertListEqual(base_node.tags, ['math'])
+        self.assertListEqual(base_node.tags, ['data'])
 
-    def test_invalid_instance(self) -> None:
+    def test_init_invalid_args(self) -> None:
         with self.assertRaisesRegex(
             InstanceError, 
             r'id_ does not match the pattern \(only letters, digits, '
@@ -33,7 +33,7 @@ class TestBaseNode(TestCase):
         ):
             _ = BaseNode('n1,', None)
 
-    def test_instance_repr(self) -> None:
+    def test_repr(self) -> None:
         base_node = BaseNode('n1', None)
         instance_repr = base_node.__repr__()
 
@@ -51,7 +51,7 @@ class TestBaseNode(TestCase):
 
 
 class TestNode(TestCase):
-    def test_base_class(self) -> None:
+    def test_new_valid_args(self) -> None:
         node = Node('n1', None)
         
         self.assertIsInstance(node, BaseNode)
