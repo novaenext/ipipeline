c9756569d0759e562caf9e8aaf361f59352e14a4
flmoro <fernandoluizmoro@gmail.com>
2021-07-29 17:36:39 -0300

    feat(sorting): add the _create_ind_node_ids func

--- ipipeline/control/sorting.py
+++ ipipeline/control/sorting.py
@@ -1,4 +1,16 @@
+from typing import Dict, List
+
 from ..exceptions import SortingError
+
+
+def _create_ind_node_ids(in_conns_qty: Dict[str, int]) -> List[str]:
+    ind_node_ids = []
+
+    for node_id, in_qty in in_conns_qty.items():
+        if in_qty == 0:
+            ind_node_ids.append(node_id)
+
+    return ind_node_ids
 
 
 def _check_diff_nodes_qty(exp_nodes_qty: int, curr_nodes_qty: int) -> None:
