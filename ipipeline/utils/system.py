ad14bea4492989b85f55279d13d9e6e6d02d301d
flmoro <fernandoluizmoro@gmail.com>
2021-07-29 11:10:59 -0300

    feat(system): add the create_directory func

--- ipipeline/utils/system.py
+++ ipipeline/utils/system.py
@@ -6,3 +6,14 @@ from ..exceptions import SystemError
 def check_inexistent_path(path: str) -> None:
     if not Path(path).exists():
         raise SystemError('inexistent path found', f'path == {path}')
+
+
+def create_directory(
+    path: str, parents: bool = False, suppressed: bool = False
+) -> None:
+    try:
+        Path(path).mkdir(parents=parents, exist_ok=suppressed)
+    except FileExistsError as error:
+        raise SystemError(
+            'directory not created', f'path == {path}'
+        ) from error
