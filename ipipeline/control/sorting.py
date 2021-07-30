44a4ab8ec75f251d826ea65a206a24a9646e380a
flmoro <fernandoluizmoro@gmail.com>
2021-07-30 09:06:39 -0300

    feat(sorting): add the _create_in_conns_qty func

--- ipipeline/control/sorting.py
+++ ipipeline/control/sorting.py
@@ -3,6 +3,22 @@ from typing import Dict, List
 from ..exceptions import SortingError
 
 
+def _create_in_conns_qty(graph: Dict[str, list]) -> Dict[str, int]:
+    in_conns_qty = dict.fromkeys(graph, 0)
+
+    for dst_node_ids in graph.values():
+        for dst_node_id in dst_node_ids:
+            try:
+                in_conns_qty[dst_node_id] += 1
+            except KeyError as error:
+                raise SortingError(
+                    'dst_node_id without src_node_id', 
+                    f'dst_node_id == {dst_node_id}'
+                ) from error
+
+    return in_conns_qty
+
+
 def _create_ind_node_ids(in_conns_qty: Dict[str, int]) -> List[str]:
     ind_node_ids = []
 
