c6fdc24a6ffbec272eec47ec129d8038e285fa70
flmoro <fernandoluizmoro@gmail.com>
2021-07-30 09:10:32 -0300

    test(test_sorting): add the TestCreateInConnsQty class

--- tests/control/test_sorting.py
+++ tests/control/test_sorting.py
@@ -2,8 +2,36 @@ from unittest import TestCase
 
 from ipipeline.exceptions import SortingError
 from ipipeline.control.sorting import (
-    _create_ind_node_ids, _check_diff_nodes_qty
+    _create_in_conns_qty, _create_ind_node_ids, _check_diff_nodes_qty
 )
+
+
+class TestCreateInConnsQty(TestCase):
+    def test_graph_with_dst_node_ids(self) -> None:
+        in_conns_qty = _create_in_conns_qty(
+            {'n1': ['n2', 'n3'], 'n2': ['n4'], 'n3': ['n4'], 'n4': []}
+        )
+
+        self.assertDictEqual(
+            in_conns_qty, {'n1': 0, 'n2': 1, 'n3': 1, 'n4': 2}
+        )
+
+    def test_graph_without_dst_node_ids(self) -> None:
+        in_conns_qty = _create_in_conns_qty(
+            {'n1': [], 'n2': [], 'n3': [], 'n4': []}
+        )
+
+        self.assertDictEqual(
+            in_conns_qty, {'n1': 0, 'n2': 0, 'n3': 0, 'n4': 0}
+        )
+
+    def test_graph_without_src_node_ids(self) -> None:
+        with self.assertRaisesRegex(
+            SortingError, r'dst_node_id without src_node_id: dst_node_id == n4'
+        ):
+            _ = _create_in_conns_qty(
+                {'n1': ['n2', 'n3'], 'n2': ['n4'], 'n3': ['n4']}
+            )
 
 
 class TestCreateIndNodeIds(TestCase):
