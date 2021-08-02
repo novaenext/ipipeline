7a14374d916e63be7c5492e1d9d9c33de4a52415
flmoro <fernandoluizmoro@gmail.com>
2021-08-02 16:59:08 -0300

    feat(exceptions): add the *Error classes of the control and utils mods

--- ipipeline/exceptions.py
+++ ipipeline/exceptions.py
@@ -7,9 +7,33 @@ class BaseError(Exception):
         return f'{self._descr}: {self._detail}'
 
 
+class CatalogError(BaseError):
+    pass
+
+
+class ExecutionError(BaseError):
+    pass
+
+
+class SortingError(BaseError):
+    pass
+
+
 class PipelineError(BaseError):
     pass
 
 
 class InstanceError(BaseError):
+    pass
+
+
+class RecordError(BaseError):
+    pass
+
+
+class SequenceError(BaseError):
+    pass
+
+
+class SystemError(BaseError):
     pass
