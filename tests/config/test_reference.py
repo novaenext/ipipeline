89dd4d01af53b25bad634f19b4e224e60e3515b3
flmoro <fernandoluizmoro@gmail.com>
2021-08-05 15:14:29 -0300

    test(config): rename the test_reference module

--- tests/config/test_reference.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-from ipipeline.config.reference import BASE_PATH
-
-
-class TestConstants(TestCase):
-    def test_base_path(self) -> None:
-        self.assertIn('ipipeline', BASE_PATH)
