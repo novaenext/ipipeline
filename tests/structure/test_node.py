4d9ad2eeddf9132bbac13d16c34898fe25f66bf4
flmoro <fernandoluizmoro@gmail.com>
2021-09-03 16:11:05 -0300

    test(TestBaseNode): remove the duplicate test methods

--- tests/structure/test_node.py
+++ tests/structure/test_node.py
@@ -1,6 +1,5 @@
 from unittest import TestCase
 
-from ipipeline.exceptions import InstanceError
 from ipipeline.structure.node import BaseNode, Node
 
 
@@ -9,12 +8,12 @@ def mock_func(param1: int) -> int:
 
 
 class TestBaseNode(TestCase):
-    def test_init_valid_args(self) -> None:
+    def test_init(self) -> None:
         base_node = BaseNode(
             'n1', 
             mock_func, 
             inputs={'param1': 7}, 
-            outputs=['return1'],
+            outputs=['return1'], 
             tags=['data']
         )
 
@@ -22,36 +21,11 @@ class TestBaseNode(TestCase):
         self.assertEqual(base_node.func, mock_func)
         self.assertDictEqual(base_node.inputs, {'param1': 7})
         self.assertListEqual(base_node.outputs, ['return1'])
-        self.assertListEqual(base_node.props, [])
         self.assertListEqual(base_node.tags, ['data'])
-
-    def test_init_invalid_args(self) -> None:
-        with self.assertRaisesRegex(
-            InstanceError, 
-            r'id_ does not match the pattern \(only letters, digits, '
-            r'underscore and/or dash\): id_ == n1,'
-        ):
-            _ = BaseNode('n1,', None)
-
-    def test_repr(self) -> None:
-        base_node = BaseNode('n1', None)
-        instance_repr = base_node.__repr__()
-
-        self.assertEqual(
-            instance_repr, 
-            'BaseNode('
-                'id_=\'n1\', '
-                'func=None, '
-                'inputs={}, '
-                'outputs=[], '
-                'props=[], '
-                'tags=[]'
-            ')'
-        )
 
 
 class TestNode(TestCase):
-    def test_new_valid_args(self) -> None:
+    def test_new(self) -> None:
         node = Node('n1', None)
-        
+
         self.assertIsInstance(node, BaseNode)
