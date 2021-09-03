61b12101063b222019e22ca4505abbf3ad009989
flmoro <fernandoluizmoro@gmail.com>
2021-09-03 14:17:01 -0300

    test(TestBaseError): update the method name

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -4,14 +4,14 @@ from ipipeline.exceptions import BaseError
 
 
 class TestBaseError(TestCase):
-    def test_init_valid_args(self) -> None:
-        error = BaseError('error found', 'error == value')
+    def test_init(self) -> None:
+        error = BaseError('error msg', 'error == value')
 
-        self.assertEqual(error._descr, 'error found')
+        self.assertEqual(error._descr, 'error msg')
         self.assertEqual(error._detail, 'error == value')
 
     def test_str(self) -> None:
-        error = BaseError('error found', 'error == value')
+        error = BaseError('error msg', 'error == value')
         error_str = error.__str__()
 
-        self.assertEqual(error_str, 'error found: error == value')
+        self.assertEqual(error_str, 'error msg: error == value')
