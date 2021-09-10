ae7a7c208bba72fbd7fef428c500dabc5d6e908d
flmoro <fernandoluizmoro@gmail.com>
2021-09-10 15:45:56 -0300

    refac(building): replace the Builder class with funcs

--- ipipeline/control/building.py
+++ ipipeline/control/building.py
@@ -1,64 +1,49 @@
-from abc import ABC, abstractmethod
 from typing import Any, Dict, List
 
-from ipipeline.control.catalog import BaseCatalog
 from ipipeline.exceptions import BuildingError
 
 
-class BaseBuilder(ABC):
-    @abstractmethod
-    def build_func_inputs(
-        self, inputs: Dict[str, Any], catalog: BaseCatalog
-    ) -> Dict[str, Any]:
-        pass
+def build_func_inputs(
+    inputs: Dict[str, Any], items: Dict[str, Any]
+) -> Dict[str, Any]:
+    func_inputs = {}
 
-    @abstractmethod
-    def build_func_outputs(
-        self, outputs: List[str], returns: Any
-    ) -> Dict[str, Any]:
-        pass
-
-
-class Builder(BaseBuilder):
-    def build_func_inputs(
-        self, inputs: Dict[str, Any], catalog: BaseCatalog
-    ) -> Dict[str, Any]:
-        func_inputs = {}
-
-        for in_key, in_value in inputs.items():
-            if isinstance(in_value, str) and in_value.startswith('c.'):
-                func_inputs[in_key] = catalog.obtain_item(
-                    in_value.replace('c.', '')
-                )
-            else:
-                func_inputs[in_key] = in_value
-
-        return func_inputs
-
-    def build_func_outputs(
-        self, outputs: List[str], returns: Any
-    ) -> Dict[str, Any]:
-        outputs_qty = len(outputs)
-
-        if outputs_qty > 0:
-            if outputs_qty == 1:
-                returns = [returns]
-
+    for in_key, in_value in inputs.items():
+        if isinstance(in_value, str) and in_value.startswith('c.'):
             try:
-                returns_qty = len(returns)
-            except TypeError:
-                returns_qty = 1
-
-            self._check_diff_outputs_qty(outputs_qty, returns_qty)
-
-            return dict(zip(outputs, returns))
+                func_inputs[in_key] = items[in_value.replace('c.', '')]
+            except KeyError as error:
+                raise BuildingError(
+                    'in_value not found in the items', 
+                    f'in_value == {in_value}'
+                ) from error
         else:
-            return {}
+            func_inputs[in_key] = in_value
 
-    def _check_diff_outputs_qty(
-        self, outputs_qty: int, returns_qty: int
-    ) -> None:
-        if outputs_qty != returns_qty:
-            raise BuildingError(
-                'outputs did not match', f'{outputs_qty} != {returns_qty}'
-            )
+    return func_inputs
+
+
+def build_func_outputs(outputs: List[str], returns: Any) -> Dict[str, Any]:
+    func_outputs = {}
+    outputs_qty = len(outputs)
+
+    if outputs_qty > 0:
+        if outputs_qty == 1:
+            returns = [returns]
+
+        try:
+            returns_qty = len(returns)
+        except TypeError:
+            returns_qty = 1
+
+        _check_diff_outputs_qty(outputs_qty, returns_qty)
+        func_outputs = dict(zip(outputs, returns))
+
+    return func_outputs
+
+
+def _check_diff_outputs_qty(outputs_qty: int, returns_qty: int) -> None:
+    if outputs_qty != returns_qty:
+        raise BuildingError(
+            'outputs did not match', f'{outputs_qty} != {returns_qty}'
+        )
