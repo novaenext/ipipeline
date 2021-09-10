2b6262cefe98e9fe107e4c7b8ad6dfac56bb5d64
flmoro <fernandoluizmoro@gmail.com>
2021-09-10 16:51:34 -0300

    fix(execute_pipeline): handle the error related to a missing node_id

--- ipipeline/control/execution.py
+++ ipipeline/control/execution.py
@@ -2,10 +2,9 @@ import logging
 from abc import ABC, abstractmethod
 from typing import Any, Callable, Dict, List
 
-from ipipeline.control.building import Builder
+from ipipeline.control.building import build_func_inputs, build_func_outputs
 from ipipeline.control.catalog import Catalog
 from ipipeline.exceptions import ExecutionError
-from ipipeline.structure.node import BaseNode
 from ipipeline.structure.pipeline import BasePipeline
 from ipipeline.utils.instance import InstanceIdentifier
 
@@ -27,9 +26,8 @@ class BaseExecutor(ABC, InstanceIdentifier):
 
 class SequentialExecutor(BaseExecutor):
     def __init__(self, id_: str, tags: List[str] = []) -> None:
-        self._flagged_nodes = {}
-        self._builder = Builder()
         self._catalog = Catalog()
+        self._flagged = {}
 
         super().__init__(id_, tags)
 
@@ -40,9 +38,9 @@ class SequentialExecutor(BaseExecutor):
     def flag_node(self, node_id: str, flag: str, status: bool) -> None:
         self._check_invalid_flag(flag)
 
-        if node_id not in self._flagged_nodes:
-            self._flagged_nodes[node_id] = {}
-        self._flagged_nodes[node_id][flag] = status
+        if node_id not in self._flagged:
+            self._flagged[node_id] = {}
+        self._flagged[node_id][flag] = status
 
     def _check_invalid_flag(self, flag: str) -> None:
         valid_flags = ['skip']
@@ -56,20 +54,25 @@ class SequentialExecutor(BaseExecutor):
         self, pipeline: BasePipeline, topo_order: List[str]
     ) -> None:
         for node_id in topo_order:
-            if not self._flagged_nodes.get(node_id, {}).get('skip', False):
+            if self._flagged.get(node_id, {}).get('skip', False):
+                continue
+
+            try:
                 node = pipeline.nodes[node_id]
-                logger.info(f'node - id: {node.id}, tags: {node.tags}')
+            except KeyError as error:
+                raise ExecutionError(
+                    'node_id not found in the pipeline.nodes', 
+                    f'node_id == {node_id}'
+                ) from error
 
-                func_inputs = self._builder.build_func_inputs(
-                    node.inputs, self._catalog
-                )
-                results = self._execute_func(node.id, node.func, func_inputs)
-                func_outputs = self._builder.build_func_outputs(
-                    node.outputs, results
-                )
+            logger.info(f'node - id: {node.id}, tags: {node.tags}')
 
-                if func_outputs:
-                    self._catalog_outputs(func_outputs)
+            func_inputs = build_func_inputs(node.inputs, self._catalog.items)
+            returns = self._execute_func(node.id, node.func, func_inputs)
+            func_outputs = build_func_outputs(node.outputs, returns)
+
+            if func_outputs:
+                self._catalog_outputs(func_outputs)
 
     def _execute_func(
         self, id_: str, func: Callable, inputs: Dict[str, Any]
@@ -78,7 +81,7 @@ class SequentialExecutor(BaseExecutor):
             return func(**inputs)
         except Exception as error:
             raise ExecutionError(
-                'func not executed', f'id == {id_}'
+                'func not executed', f'id_ == {id_}'
             ) from error
 
     def _catalog_outputs(self, outputs: Dict[str, Any]) -> None:
