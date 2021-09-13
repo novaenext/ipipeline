c23d2689a0e01ce054685cc38ccd72907ec2d021
flmoro <fernandoluizmoro@gmail.com>
2021-09-13 15:08:06 -0300

    feat(BaseExecutor): add the obtain_exe_order method

--- ipipeline/control/execution.py
+++ ipipeline/control/execution.py
@@ -4,15 +4,16 @@ from typing import Any, Dict, List
 
 from ipipeline.control.building import build_func_inputs, build_func_outputs
 from ipipeline.control.catalog import Catalog
+from ipipeline.control.sorting import sort_dag_topo
 from ipipeline.exceptions import ExecutionError
 from ipipeline.structure.pipeline import BasePipeline
-from ipipeline.utils.instance import InstanceIdentifier
+from ipipeline.utils.sequence import flatten_nested_seq
 
 
 logger = logging.getLogger(name=__name__)
 
 
-class BaseExecutor(ABC, InstanceIdentifier):
+class BaseExecutor(ABC):
     @abstractmethod
     def flag_node(self, node_id: str, flag: str, status: bool) -> None:
         pass
@@ -22,20 +23,19 @@ class BaseExecutor(ABC, InstanceIdentifier):
         pass
 
     @abstractmethod
+    def obtain_exe_order(self) -> list:
+        pass
+
+    @abstractmethod
     def execute_pipeline(self, exe_order: list) -> None:
         pass
 
 
 class SequentialExecutor(BaseExecutor):
-    def __init__(
-        self, id_: str, pipeline: BasePipeline, tags: List[str] = []
-    ) -> None:
+    def __init__(self, pipeline: BasePipeline) -> None:
         self._pipeline = pipeline
         self._catalog = Catalog()
-        self._exe_order = []
         self._flagged = {}
-
-        super().__init__(id_, tags)
 
     @property
     def catalog(self) -> Catalog:
@@ -76,6 +76,12 @@ class SequentialExecutor(BaseExecutor):
             raise ExecutionError(
                 'node not executed', f'node_id == {node_id}'
             ) from error
+
+    def obtain_exe_order(self) -> List[str]:
+        exe_order = flatten_nested_seq(sort_dag_topo(self._pipeline.graph))
+        logger.info(f'exe_order: {exe_order}')
+
+        return exe_order
 
     def execute_pipeline(self, exe_order: List[str]) -> None:
         for node_id in exe_order:
