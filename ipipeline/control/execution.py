54dabc7111be46bf96f07435cdc3c6ec33a620bf
flmoro <fernandoluizmoro@gmail.com>
2021-09-13 14:51:50 -0300

    refac(BaseExecutor): remove the pipeline param from some methods

--- ipipeline/control/execution.py
+++ ipipeline/control/execution.py
@@ -18,21 +18,21 @@ class BaseExecutor(ABC, InstanceIdentifier):
         pass
 
     @abstractmethod
-    def execute_node(
-        self, pipeline: BasePipeline, node_id: str
-    ) -> Dict[str, Any]:
+    def execute_node(self, node_id: str) -> Dict[str, Any]:
         pass
 
     @abstractmethod
-    def execute_pipeline(
-        self, pipeline: BasePipeline, topo_order: list
-    ) -> None:
+    def execute_pipeline(self, exe_order: list) -> None:
         pass
 
 
 class SequentialExecutor(BaseExecutor):
-    def __init__(self, id_: str, tags: List[str] = []) -> None:
+    def __init__(
+        self, id_: str, pipeline: BasePipeline, tags: List[str] = []
+    ) -> None:
+        self._pipeline = pipeline
         self._catalog = Catalog()
+        self._exe_order = []
         self._flagged = {}
 
         super().__init__(id_, tags)
@@ -42,11 +42,17 @@ class SequentialExecutor(BaseExecutor):
         return self._catalog
 
     def flag_node(self, node_id: str, flag: str, status: bool) -> None:
-        self._check_invalid_flag(flag)
+        try:
+            self._check_invalid_flag(flag)
+            node = self._pipeline.nodes[node_id]
 
-        if node_id not in self._flagged:
-            self._flagged[node_id] = {}
-        self._flagged[node_id][flag] = status
+            if node_id not in self._flagged:
+                self._flagged[node.id] = {}
+            self._flagged[node.id][flag] = status
+        except Exception as error:
+            raise ExecutionError(
+                'node not flagged', f'node_id == {node_id}'
+            ) from error
 
     def _check_invalid_flag(self, flag: str) -> None:
         valid_flags = ['skip']
@@ -56,11 +62,9 @@ class SequentialExecutor(BaseExecutor):
                 'flag not found in the valid_flags', f'flag == {flag}'
             )
 
-    def execute_node(
-        self, pipeline: BasePipeline, node_id: str
-    ) -> Dict[str, Any]:
+    def execute_node(self, node_id: str) -> Dict[str, Any]:
         try:
-            node = pipeline.nodes[node_id]
+            node = self._pipeline.nodes[node_id]
             logger.info(f'node - id: {node.id}, tags: {node.tags}')
 
             func_inputs = build_func_inputs(node.inputs, self._catalog.items)
@@ -73,12 +77,10 @@ class SequentialExecutor(BaseExecutor):
                 'node not executed', f'node_id == {node_id}'
             ) from error
 
-    def execute_pipeline(
-        self, pipeline: BasePipeline, topo_order: List[str]
-    ) -> None:
-        for node_id in topo_order:
+    def execute_pipeline(self, exe_order: List[str]) -> None:
+        for node_id in exe_order:
             if not self._flagged.get(node_id, {}).get('skip', False):
-                outputs = self.execute_node(pipeline, node_id)
+                outputs = self.execute_node(node_id)
 
                 if outputs:
                     for out_key, out_value in outputs.items():
