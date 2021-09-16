32355694e775099f70477da2d27b8c000a69fb63
flmoro <fernandoluizmoro@gmail.com>
2021-09-16 10:26:24 -0300

    test(test_system): add the TestCreateFile class

--- tests/utils/test_system.py
+++ tests/utils/test_system.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from unittest import TestCase
 
 from ipipeline.exceptions import SystemError
-from ipipeline.utils.system import create_directory
+from ipipeline.utils.system import create_directory, create_file
 
 
 class TestCreateDirectory(TestCase):
@@ -23,3 +23,23 @@ class TestCreateDirectory(TestCase):
         ):
             create_directory(str(self._path))
         self._path.rmdir()
+
+
+class TestCreateFile(TestCase):
+    def setUp(self) -> None:
+        self._path = Path(__file__).parents[0] / 'mock_file'
+
+    def test_inexistent_file(self) -> None:
+        create_file(str(self._path))
+
+        self.assertTrue(self._path.exists())
+        self._path.unlink()
+
+    def test_existent_file(self) -> None:
+        create_file(str(self._path))
+
+        with self.assertRaisesRegex(
+            SystemError, r'file not created: path == *'
+        ):
+            create_file(str(self._path))
+        self._path.unlink()
