de39c571c707e94f26878d1f59eb409601064b27
flmoro <fernandoluizmoro@gmail.com>
2021-09-17 17:23:50 -0300

    refac(commands): remove the execute_cli_cmds func

--- ipipeline/cli/commands.py
+++ ipipeline/cli/commands.py
@@ -3,16 +3,11 @@ from argparse import Namespace
 from ipipeline.utils.system import create_directory, create_file
 
 
-def execute_cli_cmds(cmd: str, args: Namespace) -> None:
-    if cmd == 'project':
-        _execute_project_cmd(args)
-
-
-def _execute_project_cmd(args: Namespace) -> None:
-    project_path = f'{args.path}/{args.name}'
+def _execute_project_cmd(parsed_args: Namespace) -> None:
+    project_path = f'{parsed_args.path}/{parsed_args.name}'
     create_directory(project_path)
 
-    for root_dir in [args.name, 'io', 'tests']:
+    for root_dir in [parsed_args.name, 'io', 'tests']:
         create_directory(f'{project_path}/{root_dir}')
 
     for root_file in [
@@ -26,13 +21,13 @@ def _execute_project_cmd(args: Namespace) -> None:
         create_file(f'{project_path}/{root_file}')
 
     for package_file in ['__init__.py', '__main__.py', 'exceptions.py']:
-        create_file(f'{project_path}/{args.name}/{package_file}')
+        create_file(f'{project_path}/{parsed_args.name}/{package_file}')
 
     for subpackage_dir in ['configs', 'groups', 'tasks']:
-        create_directory(f'{project_path}/{args.name}/{subpackage_dir}')
+        create_directory(f'{project_path}/{parsed_args.name}/{subpackage_dir}')
 
         for subpackage_file in ['__init__.py']:
             create_directory(
-                f'{project_path}/{args.name}/'
+                f'{project_path}/{parsed_args.name}/'
                 f'{subpackage_dir}/{subpackage_file}'
             )
