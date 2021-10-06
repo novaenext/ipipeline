80a2968352d0677eaaac0b9360e2b8340df43256
flmoro <fernandoluizmoro@gmail.com>
2021-10-06 17:17:52 -0300

    test(test_exceptions): add the Test*Error classes

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,6 +1,15 @@
 from unittest import TestCase
 
-from ipipeline.exceptions import BaseError
+from ipipeline.exceptions import (
+    BaseError, 
+    BuildingError, 
+    CatalogError, 
+    ExecutionError, 
+    SortingError, 
+    PipelineError, 
+    InstanceError, 
+    SystemError
+)
 
 
 class TestBaseError(TestCase):
@@ -15,3 +24,52 @@ class TestBaseError(TestCase):
         error_str = error.__str__()
 
         self.assertEqual(error_str, 'error msg: error == value')
+
+
+class TestBuildingError(TestCase):
+    def test_deriv(self) -> None:
+        error = BuildingError('', '')
+
+        self.assertIsInstance(error, BaseError)
+
+
+class TestCatalogError(TestCase):
+    def test_deriv(self) -> None:
+        error = CatalogError('', '')
+
+        self.assertIsInstance(error, BaseError)
+
+
+class TestExecutionError(TestCase):
+    def test_deriv(self) -> None:
+        error = ExecutionError('', '')
+
+        self.assertIsInstance(error, BaseError)
+
+
+class TestSortingError(TestCase):
+    def test_deriv(self) -> None:
+        error = SortingError('', '')
+
+        self.assertIsInstance(error, BaseError)
+
+
+class TestPipelineError(TestCase):
+    def test_deriv(self) -> None:
+        error = PipelineError('', '')
+
+        self.assertIsInstance(error, BaseError)
+
+
+class TestInstanceError(TestCase):
+    def test_deriv(self) -> None:
+        error = InstanceError('', '')
+
+        self.assertIsInstance(error, BaseError)
+
+
+class TestSystemError(TestCase):
+    def test_deriv(self) -> None:
+        error = SystemError('', '')
+
+        self.assertIsInstance(error, BaseError)
