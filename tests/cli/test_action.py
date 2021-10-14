759c743b1179b087a5cd71ee842166b062c938e6
fnnmoro <fnnmoro@gmail.com>
2021-10-14 15:54:26 -0300

    test(test_action): add the TestCreateProject class

--- tests/cli/test_action.py
+++ tests/cli/test_action.py
@@ -2,8 +2,8 @@ from pathlib import Path
 from shutil import rmtree
 from unittest import TestCase
 
-from ipipeline.exceptions import SystemError
-from ipipeline.structure.project import create_project
+from ipipeline.cli.action import create_project
+from ipipeline.exception import ActionError
 
 
 class TestCreateProject(TestCase):
@@ -14,23 +14,23 @@ class TestCreateProject(TestCase):
         rmtree(self._path)
 
     def test_inexistent_project(self) -> None:
-        create_project(str(self._path.parents[0]), 'mock_proj', 'mock_pkg')
+        create_project(str(self._path.parents[0]), 'mock_proj')
         proj_items = [
             '.gitignore', 
             'CONTRIBUTING.md', 
             'io', 
             'LICENSE.md', 
-            'mock_pkg', 
+            'mock_proj', 
             'README.md', 
-            'requirements.txt', 
+            'requirement.txt', 
             'setup.py', 
-            'tests'
+            'test'
         ]
         pkg_items = [
-            'configs', 
-            'exceptions.py', 
-            'groups', 
-            'tasks', 
+            'config', 
+            'exception.py', 
+            'group', 
+            'task', 
             '__init__.py', 
             '__main__.py'
         ]
@@ -39,17 +39,17 @@ class TestCreateProject(TestCase):
             self.assertTrue((self._path / proj_item).exists())
 
         for pkg_item in pkg_items:
-            self.assertTrue((self._path / 'mock_pkg' / pkg_item).exists())
+            self.assertTrue((self._path / 'mock_proj' / pkg_item).exists())
 
-        for pkg_dir in ['configs', 'groups', 'tasks']:
+        for pkg_dir in ['config', 'group', 'task']:
             self.assertTrue(
-                (self._path / 'mock_pkg' / pkg_dir / '__init__.py').exists()
+                (self._path / 'mock_proj' / pkg_dir / '__init__.py').exists()
             )
 
     def test_existent_project(self) -> None:
-        create_project(str(self._path.parents[0]), 'mock_proj', 'mock_pkg')
+        create_project(str(self._path.parents[0]), 'mock_proj')
 
         with self.assertRaisesRegex(
-            SystemError, r'directory not created in the file system: path == *'
+            ActionError, r'project not created in the file system: path == *'
         ):
-            create_project(str(self._path.parents[0]), 'mock_proj', 'mock_pkg')
+            create_project(str(self._path.parents[0]), 'mock_proj')
