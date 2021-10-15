a42c5697b8bddcf75c3bfc8b1962124b383ff949
fnnmoro <fnnmoro@gmail.com>
2021-10-15 14:37:18 -0300

    refac(__main__): remove the main func

--- ipipeline/__main__.py
+++ ipipeline/__main__.py
@@ -1,22 +1,7 @@
 import sys
 
-from ipipeline.cli.commands import execute_cli_cmds
-from ipipeline.cli.parsing import parse_cli_args
-
-
-def main() -> None:
-    try:
-        if len(sys.argv) > 1:
-            args = parse_cli_args(sys.argv[1:])
-            execute_cli_cmds(sys.argv[1], args)
-        else:
-            parse_cli_args(['--help'])
-    except Exception as error:
-        print('error:', ': '.join(error.args), file=sys.stderr)
-        sys.exit(1)
-
-    sys.exit(0)
+from ipipeline.cli.execution import execute_cli
 
 
 if __name__ == '__main__':
-    main()
+    execute_cli(sys.argv[1:])
