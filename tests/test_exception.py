3301362166f859c16eafd3e02057cc63bebdaf74
fnnmoro <fnnmoro@gmail.com>
2021-10-15 14:43:46 -0300

    test(TestBaseError): update the init args

--- tests/test_exception.py
+++ tests/test_exception.py
@@ -15,16 +15,16 @@ from ipipeline.exception import (
 
 class TestBaseError(TestCase):
     def test_init(self) -> None:
-        error = BaseError('error msg', 'error == value')
+        error = BaseError('error descr', 'error == value')
 
-        self.assertEqual(error._descr, 'error msg')
+        self.assertEqual(error._descr, 'error descr')
         self.assertEqual(error._detail, 'error == value')
 
     def test_str(self) -> None:
-        error = BaseError('error msg', 'error == value')
+        error = BaseError('error descr', 'error == value')
         error_str = error.__str__()
 
-        self.assertEqual(error_str, 'error msg: error == value')
+        self.assertEqual(error_str, 'error descr: error == value')
 
 
 class TestActionError(TestCase):
