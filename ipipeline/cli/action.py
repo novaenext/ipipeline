894b5c983acb09b13213295684df9b07fc0f0020
fnnmoro <fnnmoro@gmail.com>
2021-10-28 16:12:13 -0300

    refac(create_project): remove the sub_files creation loop

--- ipipeline/cli/action.py
+++ ipipeline/cli/action.py
@@ -28,26 +28,23 @@ def create_project(path: str, name: str) -> None:
     try:
         proj_path = f'{path}/{name}'
         pkg_path = f'{proj_path}/{name}'
-        create_directory(proj_path)
+        create_directory(proj_path, missing=True, suppressed=True)
 
-        for proj_dir in ['io', 'test', name]:
+        for proj_dir in ['io', 'requirement', 'test', name]:
             create_directory(f'{proj_path}/{proj_dir}')
 
         for proj_file in [
             '.gitignore', 
             'CONTRIBUTING.md', 
             'LICENSE.md', 
+            'MANIFEST.in', 
             'README.md', 
-            'requirement.txt', 
             'setup.py'
         ]:
             create_file(f'{proj_path}/{proj_file}')
 
         for pkg_dir in ['config', 'group', 'task']:
             create_directory(f'{pkg_path}/{pkg_dir}')
-
-            for sub_file in ['__init__.py']:
-                create_file(f'{pkg_path}/{pkg_dir}/{sub_file}')
 
         for pkg_file in ['exception.py', '__init__.py', '__main__.py']:
             create_file(f'{pkg_path}/{pkg_file}')
