2bf8dc77b5651831717418fd3859ca20a8039fed
fnnmoro <fnnmoro@gmail.com>
2021-11-08 16:38:41 -0300

    test(TestBuildFuncInputs): add the test_multiple_inputs method
    the existing tests were updated due to the items parameter change.

--- test/control/test_building.py
+++ test/control/test_building.py
@@ -3,47 +3,60 @@ from unittest import TestCase
 from ipipeline.control.building import (
     build_func_inputs, build_func_outputs, _check_diff_outputs_qty
 )
-from ipipeline.exception import BuildingError
+from ipipeline.control.catalog import Catalog
+from ipipeline.exception import BuildingError, CatalogError
 
 
 class TestBuildFuncInputs(TestCase):
-    def test_inputs_with_valid_items(self) -> None:
+    def setUp(self) -> None:
+        self._catalog = Catalog()
+        self._catalog._items = {'i1': 7, 'i2': 0}
+
+    def test_single_inputs(self) -> None:
         func_inputs = build_func_inputs(
-            {'in1': 'c.i1', 'in2': 'c.i2'}, {'i1': 7, 'i2': 0}
+            {'in1': 'c.i1', 'in2': 'c.i2'}, self._catalog
         )
 
         self.assertDictEqual(func_inputs, {'in1': 7, 'in2': 0})
 
-    def test_inputs_with_invalid_items(self) -> None:
-        with self.assertRaisesRegex(
-            BuildingError, 
-            r'in_value not found in the items: in_value == c\.i11'
-        ):
-            _ = build_func_inputs(
-                {'in1': 'c.i11', 'in2': 'c.i22'}, {'i1': 7, 'i2': 0}
-            )
+    def test_multiple_inputs(self) -> None:
+        func_inputs = build_func_inputs(
+            {'in1': 'c.[i1, i2]'}, self._catalog
+        )
 
-    def test_inputs_with_defaults(self) -> None:
-        func_inputs = build_func_inputs({'i1': 7, 'i2': '0'}, {})
+        self.assertDictEqual(func_inputs, {'in1': [7, 0]})
 
-        self.assertDictEqual(func_inputs, {'i1': 7, 'i2': '0'})
+    def test_default_inputs(self) -> None:
+        func_inputs = build_func_inputs(
+            {'in1': 10, 'in2': 'python'}, self._catalog
+        )
 
-    def test_inputs_without_defaults(self) -> None:
-        func_inputs = build_func_inputs({}, {'i1': 7, 'i2': 0})
+        self.assertDictEqual(func_inputs, {'in1': 10, 'in2': 'python'})
+
+    def test_empty_inputs(self) -> None:
+        func_inputs = build_func_inputs({}, self._catalog)
 
         self.assertDictEqual(func_inputs, {})
+
+    def test_inexistent_inputs(self) -> None:
+        with self.assertRaisesRegex(
+            CatalogError, r'id not found in the _items: id == i3'
+        ):
+            _ = build_func_inputs(
+                {'in1': 'c.i3', 'in2': 'c.i4'}, self._catalog
+            )
 
 
 class TestBuildFuncOutputs(TestCase):
     def test_single_outputs(self) -> None:
-        func_outputs = build_func_outputs(['o1'], [7, 0])
+        func_outputs = build_func_outputs(['out1'], [7, 0])
 
-        self.assertDictEqual(func_outputs, {'o1': [7, 0]})
+        self.assertDictEqual(func_outputs, {'out1': [7, 0]})
 
     def test_multiple_outputs(self) -> None:
-        func_outputs = build_func_outputs(['o1', 'o2'], [7, 0])
+        func_outputs = build_func_outputs(['out1', 'out2'], [7, 0])
 
-        self.assertDictEqual(func_outputs, {'o1': 7, 'o2': 0})
+        self.assertDictEqual(func_outputs, {'out1': 7, 'out2': 0})
 
     def test_empty_outputs(self) -> None:
         func_outputs = build_func_outputs([], None)
@@ -55,14 +68,14 @@ class TestBuildFuncOutputs(TestCase):
             BuildingError, 
             r'outputs_qty is not equal to the returns_qty: 2 != 1'
         ):
-            _ = build_func_outputs(['o1', 'o2'], 7)
+            _ = build_func_outputs(['out1', 'out2'], 7)
 
     def test_diff_outputs2(self) -> None:
         with self.assertRaisesRegex(
             BuildingError, 
             r'outputs_qty is not equal to the returns_qty: 2 != 3'
         ):
-            _ = build_func_outputs(['o1', 'o2'], [7, 0, 7])
+            _ = build_func_outputs(['out1', 'out2'], [7, 0, 7])
 
 
 class TestCheckDiffOutputsQty(TestCase):
