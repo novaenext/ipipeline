137da49dc3458b0f5ee9e9e362fe6a8683d5f8ad
fnnmoro <fnnmoro@gmail.com>
2021-11-08 17:47:01 -0300

    refac(control): add the imports of the BaseCatalog and Catalog classes

--- ipipeline/control/__init__.py
+++ ipipeline/control/__init__.py
@@ -4,4 +4,5 @@ The control package provides components that control the execution of
 the graph structure.
 """
 
+from ipipeline.control.catalog import BaseCatalog, Catalog
 from ipipeline.control.execution import BaseExecutor, SequentialExecutor
