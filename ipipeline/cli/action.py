c0c37a8df10d81f2965e03b79f3fb3c008d0c78f
fnnmoro <fnnmoro@gmail.com>
2021-12-01 12:03:17 -0300

    refac(execute_pipeline): add the obtain_* func calls

--- ipipeline/cli/action.py
+++ ipipeline/cli/action.py
@@ -1,10 +1,7 @@
 """Functions related to the action procedures."""
 
-import sys
-from importlib import import_module
-from pathlib import Path
-
-from ipipeline.exception import ActionError
+from ipipeline.control.execution import obtain_executor_class
+from ipipeline.structure.pipeline import obtain_pipeline
 from ipipeline.util.system import create_directory, create_file
 
 
@@ -23,37 +20,34 @@ def create_project(path: str, name: str) -> None:
 
     Raises
     ------
-    ActionError
-        Informs that the project was not created in the file system.
+    SystemError
+        Informs that the directory was not created in the file system.
+    SystemError
+        Informs that the file was not created in the file system.
     """
 
-    try:
-        proj_path = f'{path}/{name}'
-        pkg_path = f'{proj_path}/{name}'
-        create_directory(proj_path, missing=True, suppressed=True)
+    proj_path = f'{path}/{name}'
+    pkg_path = f'{proj_path}/{name}'
+    create_directory(proj_path, missing=True, suppressed=True)
 
-        for proj_dir in ['io', 'requirements', 'tests', name]:
-            create_directory(f'{proj_path}/{proj_dir}')
+    for proj_dir in ['io', 'requirements', 'tests', name]:
+        create_directory(f'{proj_path}/{proj_dir}')
 
-        for proj_file in [
-            '.gitignore', 
-            'CONTRIBUTING.md', 
-            'LICENSE.md', 
-            'MANIFEST.in', 
-            'README.md', 
-            'setup.py'
-        ]:
-            create_file(f'{proj_path}/{proj_file}')
+    for proj_file in [
+        '.gitignore', 
+        'CONTRIBUTING.md', 
+        'LICENSE.md', 
+        'MANIFEST.in', 
+        'README.md', 
+        'setup.py'
+    ]:
+        create_file(f'{proj_path}/{proj_file}')
 
-        for pkg_dir in ['config', 'group', 'task']:
-            create_directory(f'{pkg_path}/{pkg_dir}')
+    for pkg_dir in ['config', 'group', 'task']:
+        create_directory(f'{pkg_path}/{pkg_dir}')
 
-        for pkg_file in ['exception.py', '__init__.py', '__main__.py']:
-            create_file(f'{pkg_path}/{pkg_file}')
-    except Exception as error:
-        raise ActionError(
-            'project not created in the file system', f'path == {path}'
-        ) from error
+    for pkg_file in ['exception.py', '__init__.py', '__main__.py']:
+        create_file(f'{pkg_path}/{pkg_file}')
 
 
 def execute_pipeline(mod_name: str, func_name: str, exe_type: str) -> None:
@@ -65,38 +59,24 @@ def execute_pipeline(mod_name: str, func_name: str, exe_type: str) -> None:
     Parameters
     ----------
     mod_name : str
-        Name of the module.
+        Name of the module in absolute terms (pkg.mod).
     func_name : str
         Name of the function.
     exe_type : {'sequential'}
-        Type of the executor.
+        Type of the executor class.
 
         sequential: executes a pipeline sequentially.
 
     Raises
     ------
-    ActionError
+    PipelineError
         Informs that the func_name was not found in the module.
-    ActionError
-        Informs that the exe_type was not found in the module.
+    ExecutionError
+        Informs that the type was not found in the executors.
+    ExecutionError
+        Informs that the node was not executed by the executor.
     """
 
-    sys.path.append(str(Path(f'./{mod_name.split(".")[0]}').resolve()))
-
-    try:
-        pipeline = getattr(import_module(mod_name), func_name)()
-    except (ModuleNotFoundError, AttributeError) as error:
-        raise ActionError(
-            'func_name not found in the module', f'func_name == {func_name}'
-        ) from error
-
-    try:
-        executor = getattr(
-            import_module('ipipeline.control.execution'), 
-            f'{exe_type.capitalize()}Executor'
-        )(pipeline)
-        executor.execute_pipeline(executor.obtain_topo_order())
-    except AttributeError as error:
-        raise ActionError(
-            'exe_type not found in the module', f'exe_type == {exe_type}'
-        ) from error
+    pipeline = obtain_pipeline(mod_name, func_name)
+    executor = obtain_executor_class(exe_type)(pipeline)
+    executor.execute_pipeline(executor.obtain_topo_order())
