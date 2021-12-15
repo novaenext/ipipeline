e2039eb35d3b4a9a4f353d6da3d061aedc6f5973
fnnmoro <fnnmoro@gmail.com>
2021-12-15 17:30:09 -0300

    test(test_instance): add the TestCheckNoneArg class

--- tests/util/test_instance.py
+++ tests/util/test_instance.py
@@ -1,36 +1,53 @@
 from unittest import TestCase
 
 from ipipeline.exception import InstanceError
-from ipipeline.util.instance import Identification, build_repr, obtain_instance
+from ipipeline.util.instance import (
+    Identification, check_none_arg, build_repr, obtain_instance
+)
 
 
 class TestIdentification(TestCase):
     def test_init(self) -> None:
-        identifier = Identification('i1', tags=['t1', 't2'])
+        identification = Identification('i1', tags=['t1', 't2'])
 
-        self.assertEqual(identifier.id, 'i1')
-        self.assertListEqual(identifier.tags, ['t1', 't2'])
+        self.assertEqual(identification.id, 'i1')
+        self.assertListEqual(identification.tags, ['t1', 't2'])
 
     def test_check_valid_id(self) -> None:
-        id = Identification._check_valid_id(None, 'i1')
+        identification = Identification('i1')
+        id = identification._check_valid_id('i1')
 
         self.assertEqual(id, 'i1')
 
     def test_check_invalid_id(self) -> None:
+        identification = Identification('i1')
+
         with self.assertRaisesRegex(
             InstanceError, 
             r'id not validated according to the pattern \(letters, '
             r'digits, underscores and/or dashes\): id == i\.1'
         ):
-            _ = Identification._check_valid_id(None, 'i.1')
+            _ = identification._check_valid_id('i.1')
 
     def test_repr(self) -> None:
-        identifier = Identification('i1', tags=['t1', 't2'])
-        repr = identifier.__repr__()
+        identification = Identification('i1', tags=['t1', 't2'])
+        repr = identification.__repr__()
 
         self.assertEqual(
             repr, 'Identification(id=\'i1\', tags=[\'t1\', \'t2\'])'
         )
+
+
+class TestCheckNoneArg(TestCase):
+    def test_arg_with_none(self) -> None:
+        arg = check_none_arg(None, 'a1')
+
+        self.assertEqual(arg, 'a1')
+
+    def test_arg_without_none(self) -> None:
+        arg = check_none_arg('a1', 'a2')
+
+        self.assertEqual(arg, 'a1')
 
 
 class MockClass1:
@@ -56,22 +73,22 @@ class MockClass4:
 
 
 class TestBuildRepr(TestCase):
-    def test_instance_with_params_with_attrs(self) -> None:
+    def test_with_params_with_attrs(self) -> None:
         repr = build_repr(MockClass1(1))
 
         self.assertEqual(repr, 'MockClass1(param1=1, param2=\'2\')')
 
-    def test_instance_with_params_without_attrs(self) -> None:
+    def test_with_params_without_attrs(self) -> None:
         repr = build_repr(MockClass2(1))
 
         self.assertEqual(repr, 'MockClass2(param1=None, param2=None)')
 
-    def test_instance_without_params_with_attrs(self) -> None:
+    def test_without_params_with_attrs(self) -> None:
         repr = build_repr(MockClass3())
 
         self.assertEqual(repr, 'MockClass3()')
 
-    def test_instance_without_params_without_attrs(self) -> None:
+    def test_without_params_without_attrs(self) -> None:
         repr = build_repr(MockClass4())
 
         self.assertEqual(repr, 'MockClass4()')
@@ -79,9 +96,9 @@ class TestBuildRepr(TestCase):
 
 class TestObtainInstance(TestCase):
     def test_valid_names(self) -> None:
-        mock_class = obtain_instance('tests.util.test_instance', 'MockClass1')
+        instance = obtain_instance('tests.util.test_instance', 'MockClass1')
 
-        self.assertEqual(mock_class.__name__, 'MockClass1')
+        self.assertEqual(instance.__name__, 'MockClass1')
 
     def test_invalid_mod_name(self) -> None:
         with self.assertRaisesRegex(
