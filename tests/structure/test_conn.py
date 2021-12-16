101196a0e631bdf527bbc70a8c3cb3247b343cef
fnnmoro <fnnmoro@gmail.com>
2021-12-16 11:00:07 -0300

    test(test_conn): remove the TestBaseConn class

--- tests/structure/test_conn.py
+++ tests/structure/test_conn.py
@@ -1,21 +1,20 @@
 from unittest import TestCase
 
-from ipipeline.structure.conn import BaseConn, Conn
-
-
-class TestBaseConn(TestCase):
-    def test_init(self) -> None:
-        base_conn = BaseConn('c1', 'n1', 'n2', tags=['data'])
-
-        self.assertEqual(base_conn.id, 'c1')
-        self.assertEqual(base_conn.src_id, 'n1')
-        self.assertEqual(base_conn.dst_id, 'n2')
-        self.assertEqual(base_conn.value, None)
-        self.assertListEqual(base_conn.tags, ['data'])
+from ipipeline.structure.conn import Conn
 
 
 class TestConn(TestCase):
-    def test_deriv(self) -> None:
-        conn = Conn('c1', 'n1', 'n2')
+    def test_init(self) -> None:
+        conn = Conn('c1', 'n1', 'n2', tags=['data'])
 
-        self.assertIsInstance(conn, BaseConn)
+        self.assertEqual(conn.id, 'c1')
+        self.assertEqual(conn.src_id, 'n1')
+        self.assertEqual(conn.dst_id, 'n2')
+        self.assertEqual(conn.power, None)
+        self.assertListEqual(conn.tags, ['data'])
+
+    def test_defaults(self) -> None:
+        conn1 = Conn('c1', 'n1', 'n2')
+        conn2 = Conn('c2', 'n2', 'n3')
+
+        self.assertIs(conn1.power, conn2.power)
