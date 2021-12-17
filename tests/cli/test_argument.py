feb84ebc305d551ba1aac5451bab2a976b19cdfd
fnnmoro <fnnmoro@gmail.com>
2021-12-17 08:35:18 -0300

    test(test_argument): remove the TestBaseArgument class

--- tests/cli/test_argument.py
+++ tests/cli/test_argument.py
@@ -1,21 +1,14 @@
 from unittest import TestCase
 
-from ipipeline.cli.argument import BaseArgument, Argument
-
-
-class TestBaseArgument(TestCase):
-    def test_init(self) -> None:
-        base_arg = BaseArgument('a1', 'arg descr', 'store', str, param1=7)
-
-        self.assertEqual(base_arg.name, 'a1')
-        self.assertEqual(base_arg.descr, 'arg descr')
-        self.assertEqual(base_arg.action, 'store')
-        self.assertEqual(base_arg.type, str)
-        self.assertDictEqual(base_arg.key_args, {'param1': 7})
+from ipipeline.cli.argument import Argument
 
 
 class TestArgument(TestCase):
-    def test_deriv(self) -> None:
-        arg = Argument('a1', 'arg descr', 'store', str)
+    def test_init(self) -> None:
+        arg = Argument('a1', 'arg descr', 'store', str, param1=7)
 
-        self.assertIsInstance(arg, BaseArgument)
+        self.assertEqual(arg.name, 'a1')
+        self.assertEqual(arg.descr, 'arg descr')
+        self.assertEqual(arg.action, 'store')
+        self.assertEqual(arg.type, str)
+        self.assertDictEqual(arg.key_args, {'param1': 7})
