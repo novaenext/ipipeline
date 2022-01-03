b12b9a18e48a9bcd1ed3d648b0c35369270cf369
fnnmoro <fnnmoro@gmail.com>
2022-01-03 11:05:54 -0300

    refac(Conn): rename the _src_id and _dst_id attrs

--- ipipeline/structure/conn.py
+++ ipipeline/structure/conn.py
@@ -6,18 +6,18 @@ from ipipeline.structure.info import Info
 
 
 class Conn(Info):
-    """Stores a reference between two nodes.
+    """Stores a relation between two nodes.
 
-    The reference defines the dependency between the nodes, therefore the 
+    The relation defines the dependency between the nodes, therefore the 
     destination node depends on the source node.
 
     Attributes
     ----------
     _id : str
         ID of the connection.
-    _src_id : str
+    _src_node_id : str
         ID of the source node.
-    _dst_id : str
+    _dst_node_id : str
         ID of the destination node.
     _power : Any
         Power of the connection that indicates its strength.
@@ -28,8 +28,8 @@ class Conn(Info):
     def __init__(
         self, 
         id: str, 
-        src_id: str, 
-        dst_id: str, 
+        src_node_id: str, 
+        dst_node_id: str, 
         power: Any = None, 
         tags: List[str] = None
     ) -> None:
@@ -39,9 +39,9 @@ class Conn(Info):
         ----------
         id : str
             ID of the connection.
-        src_id : str
+        src_node_id : str
             ID of the source node.
-        dst_id : str
+        dst_node_id : str
             ID of the destination node.
         power : Any
             Power of the connection that indicates its strength.
@@ -54,35 +54,35 @@ class Conn(Info):
             Informs that the id was not validated according to the pattern.
         """
 
-        self._src_id = src_id
-        self._dst_id = dst_id
+        self._src_node_id = src_node_id
+        self._dst_node_id = dst_node_id
         self._power = power
 
         super().__init__(id, tags=tags)
 
     @property
-    def src_id(self) -> str:
-        """Obtains the _src_id attribute.
+    def src_node_id(self) -> str:
+        """Obtains the _src_node_id attribute.
 
         Returns
         -------
-        src_id : str
+        src_node_id : str
             ID of the source node.
         """
 
-        return self._src_id
+        return self._src_node_id
 
     @property
-    def dst_id(self) -> str:
-        """Obtains the _dst_id attribute.
+    def dst_node_id(self) -> str:
+        """Obtains the _dst_node_id attribute.
 
         Returns
         -------
-        dst_id : str
+        dst_node_id : str
             ID of the destination node.
         """
 
-        return self._dst_id
+        return self._dst_node_id
 
     @property
     def power(self) -> Any:
