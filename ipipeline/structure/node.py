364c698a95ab52e4fc44e0fe7f22983ca7cb40d4
fnnmoro <fnnmoro@gmail.com>
2022-01-03 11:06:52 -0300

    refac(Node): rename the _func attr

--- ipipeline/structure/node.py
+++ ipipeline/structure/node.py
@@ -7,26 +7,26 @@ from ipipeline.utils.instance import check_none_arg
 
 
 class Node(Info):
-    """Stores a function and its specifications.
+    """Stores a task and its specifications.
 
     The specifications are used to build the inputs and outputs to execute 
-    the function that is bound to the node.
+    the task that is bound to the node.
 
     Attributes
     ----------
     _id : str
         ID of the node.
-    _func : Callable
-        Function that represents an execution unit.
+    _task : Callable
+        Task that represents an execution unit.
     _inputs : Dict[str, Any]
-        Inputs of the function. The keys are the function parameters and 
+        Inputs of the task. The keys are the function parameters and 
         the values are any default values and/or placeholders for the 
         catalog items.
 
         'c.<item_id>': obtains a single item.
         'c.[<item_id>, ..., <item_id>]': obtains multiple items.
     _outputs : List[str]
-        Outputs of the function. The outputs must match the returns in 
+        Outputs of the task. The outputs must match the returns in 
         terms of length. If one output is expected, the return can be of 
         any type, however, in cases with more than one output, the returns 
         must be a sequence.
@@ -37,7 +37,7 @@ class Node(Info):
     def __init__(
         self, 
         id: str, 
-        func: Callable, 
+        task: Callable, 
         inputs: Dict[str, Any] = None, 
         outputs: List[str] = None, 
         tags: List[str] = None
@@ -48,17 +48,17 @@ class Node(Info):
         ----------
         id : str
             ID of the node.
-        func : Callable
-            Function that represents an execution unit.
+        task : Callable
+            Task that represents an execution unit.
         inputs : Dict[str, Any], default=None
-            Inputs of the function. The keys are the function parameters and 
+            Inputs of the task. The keys are the function parameters and 
             the values are any default values and/or placeholders for the 
             catalog items.
 
             'c.<item_id>': obtains a single item.
             'c.[<item_id>, ..., <item_id>]': obtains multiple items.
         outputs : List[str], default=None
-            Outputs of the function. The outputs must match the returns in 
+            Outputs of the task. The outputs must match the returns in 
             terms of length. If one output is expected, the return can be of 
             any type, however, in cases with more than one output, the returns 
             must be a sequence.
@@ -71,23 +71,23 @@ class Node(Info):
             Informs that the id was not validated according to the pattern.
         """
 
-        self._func = func
+        self._task = task
         self._inputs = check_none_arg(inputs, {})
         self._outputs = check_none_arg(outputs, [])
 
         super().__init__(id, tags=tags)
 
     @property
-    def func(self) -> Callable:
-        """Obtains the _func attribute.
+    def task(self) -> Callable:
+        """Obtains the _task attribute.
 
         Returns
         -------
-        func : Callable
-            Function that represents an execution unit.
+        task : Callable
+            Task that represents an execution unit.
         """
 
-        return self._func
+        return self._task
 
     @property
     def inputs(self) -> Dict[str, Any]:
@@ -96,7 +96,7 @@ class Node(Info):
         Returns
         -------
         inputs : Dict[str, Any]
-            Inputs of the function. The keys are the function parameters and 
+            Inputs of the task. The keys are the function parameters and 
             the values are any default values and/or placeholders for the 
             catalog items.
 
@@ -113,7 +113,7 @@ class Node(Info):
         Returns
         -------
         outputs : List[str]
-            Outputs of the function. The outputs must match the returns in 
+            Outputs of the task. The outputs must match the returns in 
             terms of length. If one output is expected, the return can be of 
             any type, however, in cases with more than one output, the returns 
             must be a sequence.
