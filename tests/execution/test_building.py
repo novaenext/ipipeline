9e13278a209110e70459cf3c5e04492e26db685e
fnnmoro <fnnmoro@gmail.com>
2022-01-03 11:18:38 -0300

    test(test_building): rename the build_func_inputs/outputs func calls

--- tests/execution/test_building.py
+++ tests/execution/test_building.py
@@ -1,25 +1,25 @@
 from unittest import TestCase
 
 from ipipeline.execution.building import (
-    build_func_inputs, build_func_outputs, _check_diff_outputs_qty
+    build_task_inputs, build_task_outputs, _check_diff_outputs_qty
 )
 from ipipeline.exceptions import BuildingError, CatalogError
 from ipipeline.structure.catalog import Catalog
 
 
-class TestBuildFuncInputs(TestCase):
+class TestBuildTaskInputs(TestCase):
     def setUp(self) -> None:
         self._catalog = Catalog('c1', items={'i1': 7, 'i2': 0})
 
     def test_single_placeholders(self) -> None:
-        func_inputs = build_func_inputs(
+        func_inputs = build_task_inputs(
             {'in1': 'c.i1', 'in2': 'c.i2'}, self._catalog
         )
 
         self.assertDictEqual(func_inputs, {'in1': 7, 'in2': 0})
 
     def test_multiple_placeholders(self) -> None:
-        func_inputs = build_func_inputs(
+        func_inputs = build_task_inputs(
             {'in1': 'c.[i1, i2]', 'in2': 'c.[i1]'}, self._catalog
         )
 
@@ -29,36 +29,36 @@ class TestBuildFuncInputs(TestCase):
         with self.assertRaisesRegex(
             CatalogError, r'id not found in the _items: id == i3'
         ):
-            _ = build_func_inputs(
+            _ = build_task_inputs(
                 {'in1': 'c.i3', 'in2': 'c.[]'}, self._catalog
             )
 
     def test_default_values(self) -> None:
-        func_inputs = build_func_inputs(
+        func_inputs = build_task_inputs(
             {'in1': 10, 'in2': 'python'}, self._catalog
         )
 
         self.assertDictEqual(func_inputs, {'in1': 10, 'in2': 'python'})
 
     def test_empty_values(self) -> None:
-        func_inputs = build_func_inputs({}, self._catalog)
+        func_inputs = build_task_inputs({}, self._catalog)
 
         self.assertDictEqual(func_inputs, {})
 
 
-class TestBuildFuncOutputs(TestCase):
+class TestBuildTaskOutputs(TestCase):
     def test_single_outputs(self) -> None:
-        func_outputs = build_func_outputs(['out1'], [7, 0])
+        func_outputs = build_task_outputs(['out1'], [7, 0])
 
         self.assertDictEqual(func_outputs, {'out1': [7, 0]})
 
     def test_multiple_outputs(self) -> None:
-        func_outputs = build_func_outputs(['out1', 'out2'], [7, 0])
+        func_outputs = build_task_outputs(['out1', 'out2'], [7, 0])
 
         self.assertDictEqual(func_outputs, {'out1': 7, 'out2': 0})
 
     def test_empty_outputs(self) -> None:
-        func_outputs = build_func_outputs([], None)
+        func_outputs = build_task_outputs([], None)
 
         self.assertDictEqual(func_outputs, {})
 
@@ -67,14 +67,14 @@ class TestBuildFuncOutputs(TestCase):
             BuildingError, 
             r'outputs_qty is not equal to the returns_qty: 2 != 1'
         ):
-            _ = build_func_outputs(['out1', 'out2'], 7)
+            _ = build_task_outputs(['out1', 'out2'], 7)
 
     def test_invalid_outputs2(self) -> None:
         with self.assertRaisesRegex(
             BuildingError, 
             r'outputs_qty is not equal to the returns_qty: 2 != 3'
         ):
-            _ = build_func_outputs(['out1', 'out2'], [7, 0, 7])
+            _ = build_task_outputs(['out1', 'out2'], [7, 0, 7])
 
 
 class TestCheckDiffOutputsQty(TestCase):
