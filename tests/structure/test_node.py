7f5a62acf72bed7ff1e208b8c4b659b711e826d9
fnnmoro <fnnmoro@gmail.com>
2022-01-03 11:17:57 -0300

    test(TestNode): rename the func attr

--- tests/structure/test_node.py
+++ tests/structure/test_node.py
@@ -7,25 +7,25 @@ class TestNode(TestCase):
     def test_init(self) -> None:
         node = Node(
             'n1', 
-            mock_func, 
+            mock_task, 
             inputs={'param1': 7}, 
             outputs=['return1'], 
             tags=['data']
         )
 
         self.assertEqual(node.id, 'n1')
-        self.assertEqual(node.func, mock_func)
+        self.assertEqual(node.task, mock_task)
         self.assertDictEqual(node.inputs, {'param1': 7})
         self.assertListEqual(node.outputs, ['return1'])
         self.assertListEqual(node.tags, ['data'])
 
     def test_defaults(self) -> None:
-        node1 = Node('n1', mock_func) 
-        node2 = Node('n2', mock_func) 
+        node1 = Node('n1', mock_task) 
+        node2 = Node('n2', mock_task) 
 
         self.assertIsNot(node1.inputs, node2.inputs)
         self.assertIsNot(node1.outputs, node2.outputs)
 
 
-def mock_func(param1: int) -> int:
+def mock_task(param1: int) -> int:
     return param1
