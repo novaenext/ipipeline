355490ce741b30c9817f8b180360d13ddc31f8e8
fnnmoro <fnnmoro@gmail.com>
2022-05-23 17:01:52 -0300

    test(test_instance): rename the TestBuildInstRepr class and its methods

--- tests/utils/test_instance.py
+++ tests/utils/test_instance.py
@@ -1,24 +1,41 @@
 from unittest import TestCase
+from typing import List
 
 from ipipeline.exceptions import InstanceError
-from ipipeline.utils.instance import build_inst_repr, obtain_mod_inst
+from ipipeline.utils.instance import build_repr, obtain_mod_inst
 
 
 class MockClass1:
-    def __init__(self, param1: int, param2: str = '2') -> None:
+    def __init__(
+        self, 
+        param1: int, 
+        param2: str, 
+        param3: List[int] = [3], 
+        param4: List[str] = ['4']
+    ) -> None:
         self._param1 = param1
-        self.param2 = param2
+        self._param2 = param2
+        self.param3 = param3
+        self.param4 = param4
 
 
 class MockClass2:
-    def __init__(self, param1: int, param2: str = '2') -> None:
+    def __init__(
+        self, 
+        param1: int, 
+        param2: str, 
+        param3: List[int] = [3], 
+        param4: List[str] = ['4']
+    ) -> None:
         pass
 
 
 class MockClass3:
     def __init__(self) -> None:
         self._param1 = 1
-        self.param2 = '2'
+        self._param2 = '2'
+        self.param3 = [3]
+        self.param4 = ['4']
 
 
 class MockClass4:
@@ -26,24 +43,30 @@ class MockClass4:
         pass
 
 
-class TestBuildInstRepr(TestCase):
-    def test_inst_with_params_with_attrs(self) -> None:
-        repr = build_inst_repr(MockClass1(1))
+class TestBuildRepr(TestCase):
+    def test_build_repr__inst_wi_params_wi_attrs(self) -> None:
+        repr = build_repr(MockClass1(1, '2'))
 
-        self.assertEqual(repr, 'MockClass1(param1=1, param2=\'2\')')
+        self.assertEqual(
+            repr, 
+            'MockClass1(param1=1, param2=\'2\', param3=[3], param4=[\'4\'])'
+        )
 
-    def test_inst_with_params_without_attrs(self) -> None:
-        repr = build_inst_repr(MockClass2(1))
+    def test_build_repr__inst_wi_params_wo_attrs(self) -> None:
+        repr = build_repr(MockClass2(1, '2'))
 
-        self.assertEqual(repr, 'MockClass2(param1=None, param2=None)')
+        self.assertEqual(
+            repr, 
+            'MockClass2(param1=None, param2=None, param3=None, param4=None)'
+        )
 
-    def test_inst_without_params_with_attrs(self) -> None:
-        repr = build_inst_repr(MockClass3())
+    def test_build_repr__inst_wo_params_wi_attrs(self) -> None:
+        repr = build_repr(MockClass3())
 
         self.assertEqual(repr, 'MockClass3()')
 
-    def test_inst_without_params_without_attrs(self) -> None:
-        repr = build_inst_repr(MockClass4())
+    def test_build_repr__inst_wo_params_wo_attrs(self) -> None:
+        repr = build_repr(MockClass4())
 
         self.assertEqual(repr, 'MockClass4()')
 
