85dcc74f848678712bedfdf4fe4bb7b3066b851f
fnnmoro <fnnmoro@gmail.com>
2022-05-30 16:40:29 -0300

    test(TestCheckNone): rename the data param

--- tests/utils/test_checking.py
+++ tests/utils/test_checking.py
@@ -4,12 +4,12 @@ from ipipeline.utils.checking import check_none
 
 
 class TestCheckNone(TestCase):
-    def test_check_none__data_eq_none(self) -> None:
-        data = check_none(None, 'd1')
+    def test_check_none__item_eq_none(self) -> None:
+        item = check_none(None, 'i1')
 
-        self.assertEqual(data, 'd1')
+        self.assertEqual(item, 'i1')
 
-    def test_check_none__data_ne_none(self) -> None:
-        data = check_none('d1', 'd2')
+    def test_check_none__item_ne_none(self) -> None:
+        item = check_none('i1', 'i2')
 
-        self.assertEqual(data, 'd1')
+        self.assertEqual(item, 'i1')
