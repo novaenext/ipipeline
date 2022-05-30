786a0d4ba3c5814731089ac235c3f1fd766b5326
fnnmoro <fnnmoro@gmail.com>
2022-05-30 17:09:20 -0300

    test(test_system): rename the create_* funcs

--- tests/utils/test_system.py
+++ tests/utils/test_system.py
@@ -2,10 +2,10 @@ from pathlib import Path
 from unittest import TestCase
 
 from ipipeline.exceptions import SystemError
-from ipipeline.utils.system import create_directory, create_file
+from ipipeline.utils.system import build_directory, build_file
 
 
-class TestCreateDirectory(TestCase):
+class TestBuildDirectory(TestCase):
     def setUp(self) -> None:
         self._path = Path(__file__).resolve().parents[0] / 'mock_dir'
 
@@ -13,20 +13,20 @@ class TestCreateDirectory(TestCase):
         self._path.rmdir()
 
     def test_inexistent_directory(self) -> None:
-        create_directory(str(self._path), missing=False, suppressed=False)
+        build_directory(str(self._path), missing=False, suppressed=False)
 
         self.assertTrue(self._path.exists())
 
     def test_existent_directory(self) -> None:
-        create_directory(str(self._path), missing=False, suppressed=False)
+        build_directory(str(self._path), missing=False, suppressed=False)
 
         with self.assertRaisesRegex(
             SystemError, r'directory not created in the file system: path == *'
         ):
-            create_directory(str(self._path), missing=False, suppressed=False)
+            build_directory(str(self._path), missing=False, suppressed=False)
 
 
-class TestCreateFile(TestCase):
+class TestBuildFile(TestCase):
     def setUp(self) -> None:
         self._path = Path(__file__).resolve().parents[0] / 'mock_file'
 
@@ -34,14 +34,14 @@ class TestCreateFile(TestCase):
         self._path.unlink()
 
     def test_inexistent_file(self) -> None:
-        create_file(str(self._path), suppressed=False)
+        build_file(str(self._path), suppressed=False)
 
         self.assertTrue(self._path.exists())
 
     def test_existent_file(self) -> None:
-        create_file(str(self._path), suppressed=False)
+        build_file(str(self._path), suppressed=False)
 
         with self.assertRaisesRegex(
             SystemError, r'file not created in the file system: path == *'
         ):
-            create_file(str(self._path), suppressed=False)
+            build_file(str(self._path), suppressed=False)
