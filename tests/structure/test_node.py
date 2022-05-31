63dad90287dfe61f1e30f61054c95427e6196bd2
fnnmoro <fnnmoro@gmail.com>
2022-05-31 17:14:23 -0300

    test(TestNode): delete the test_defaults method

--- tests/structure/test_node.py
+++ tests/structure/test_node.py
@@ -4,28 +4,20 @@ from ipipeline.structure.node import Node
 
 
 class TestNode(TestCase):
-    def test_init(self) -> None:
+    def setUp(self) -> None:
+        self._mock_task = lambda p: p
+
+    def test_init__task_eq_callable__in_eq_dict__out_eq_list(self) -> None:
         node = Node(
             'n1', 
-            mock_task, 
-            inputs={'param1': 7}, 
-            outputs=['return1'], 
+            self._mock_task, 
+            inputs={'i1': 2}, 
+            outputs=['o1'], 
             tags=['t1']
         )
 
         self.assertEqual(node.id, 'n1')
-        self.assertEqual(node.task, mock_task)
-        self.assertDictEqual(node.inputs, {'param1': 7})
-        self.assertListEqual(node.outputs, ['return1'])
+        self.assertEqual(node.task, self._mock_task)
+        self.assertDictEqual(node.inputs, {'i1': 2})
+        self.assertListEqual(node.outputs, ['o1'])
         self.assertListEqual(node.tags, ['t1'])
-
-    def test_defaults(self) -> None:
-        node1 = Node('n1', mock_task)
-        node2 = Node('n2', mock_task)
-
-        self.assertIsNot(node1.inputs, node2.inputs)
-        self.assertIsNot(node1.outputs, node2.outputs)
-
-
-def mock_task(param1: int) -> int:
-    return param1
