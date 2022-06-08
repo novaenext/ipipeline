94a727fd3b69f3cc57a35d4e399934d46b85bab8
fnnmoro <fnnmoro@gmail.com>
2022-06-08 10:48:56 -0300

    test(TestNode): add the test_get* and test_set* methods

--- tests/structure/test_node.py
+++ tests/structure/test_node.py
@@ -5,19 +5,48 @@ from ipipeline.structure.node import Node
 
 class TestNode(TestCase):
     def setUp(self) -> None:
-        self._mock_task = lambda p: p
+        self._mock_tasks = [lambda p1: p1, lambda p2: p2]
 
-    def test_init__task_eq_callable__in_eq_dict__out_eq_list(self) -> None:
+    def test_init__task_eq_call__inputs_eq_dict__outputs_eq_list(self) -> None:
         node = Node(
             'n1', 
-            self._mock_task, 
+            self._mock_tasks[0], 
             inputs={'i1': 2}, 
             outputs=['o1'], 
             tags=['t1']
         )
 
-        self.assertEqual(node.id, 'n1')
-        self.assertEqual(node.task, self._mock_task)
+        self.assertEqual(node._id, 'n1')
+        self.assertEqual(node._task, self._mock_tasks[0])
+        self.assertDictEqual(node._inputs, {'i1': 2})
+        self.assertListEqual(node._outputs, ['o1'])
+        self.assertListEqual(node._tags, ['t1'])
+
+    def test_get__task_eq_call__inputs_eq_dict__outputs_eq_list(self) -> None:
+        node = Node(
+            'n1', 
+            self._mock_tasks[0], 
+            inputs={'i1': 2}, 
+            outputs=['o1'], 
+            tags=['t1']
+        )
+
+        self.assertEqual(node.task, self._mock_tasks[0])
         self.assertDictEqual(node.inputs, {'i1': 2})
         self.assertListEqual(node.outputs, ['o1'])
-        self.assertListEqual(node.tags, ['t1'])
+
+    def test_set__task_eq_call__inputs_eq_dict__outputs_eq_list(self) -> None:
+        node = Node(
+            'n1', 
+            self._mock_tasks[0], 
+            inputs={'i1': 2}, 
+            outputs=['o1'], 
+            tags=['t1']
+        )
+        node.task = self._mock_tasks[1]
+        node.inputs = {'i2': 4}
+        node.outputs = ['o2']
+
+        self.assertEqual(node.task, self._mock_tasks[1])
+        self.assertDictEqual(node.inputs, {'i2': 4})
+        self.assertListEqual(node.outputs, ['o2'])
