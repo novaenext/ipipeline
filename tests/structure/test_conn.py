0cb48b0445fa7931b57f4686df6ad2decf92c584
fnnmoro <fnnmoro@gmail.com>
2022-06-08 09:48:30 -0300

    test(TestConn): add the test_get* and test_set* methods

--- tests/structure/test_conn.py
+++ tests/structure/test_conn.py
@@ -4,10 +4,24 @@ from ipipeline.structure.conn import Conn
 
 
 class TestConn(TestCase):
-    def test_init__src_node_id_eq_str__dst_node_id_eq_str(self) -> None:
+    def test_init__src_id_eq_str__dst_id_eq_str(self) -> None:
         conn = Conn('c1', 'n1', 'n2', tags=['t1'])
 
-        self.assertEqual(conn.id, 'c1')
-        self.assertEqual(conn.src_node_id, 'n1')
-        self.assertEqual(conn.dst_node_id, 'n2')
-        self.assertListEqual(conn.tags, ['t1'])
+        self.assertEqual(conn._id, 'c1')
+        self.assertEqual(conn._src_id, 'n1')
+        self.assertEqual(conn._dst_id, 'n2')
+        self.assertListEqual(conn._tags, ['t1'])
+
+    def test_get__src_id_eq_str__dst_id_eq_str(self) -> None:
+        conn = Conn('c1', 'n1', 'n2', tags=['t1'])
+
+        self.assertEqual(conn.src_id, 'n1')
+        self.assertEqual(conn.dst_id, 'n2')
+
+    def test_set__src_id_eq_str__dst_id_eq_str(self) -> None:
+        conn = Conn('c1', 'n1', 'n2', tags=['t1'])
+        conn.src_id = 'n3'
+        conn.dst_id = 'n4'
+
+        self.assertEqual(conn.src_id, 'n3')
+        self.assertEqual(conn.dst_id, 'n4')
