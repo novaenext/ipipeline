1abb3e55b4131a4390977597c2af26349a5a90eb
fnnmoro <fnnmoro@gmail.com>
2022-06-08 09:47:34 -0300

    feat(Conn): add the src_id and dst_id setters

--- ipipeline/structure/conn.py
+++ ipipeline/structure/conn.py
@@ -65,6 +65,18 @@ class Conn(Info):
 
         return self._src_id
 
+    @src_id.setter
+    def src_id(self, src_id: str) -> None:
+        """Sets the _src_id attribute.
+
+        Parameters
+        ----------
+        src_id : str
+            ID of the source node.
+        """
+
+        self._src_id = src_id
+
     @property
     def dst_id(self) -> str:
         """Gets the _dst_id attribute.
@@ -76,3 +88,15 @@ class Conn(Info):
         """
 
         return self._dst_id
+
+    @dst_id.setter
+    def dst_id(self, dst_id: str) -> None:
+        """Sets the _dst_id attribute.
+
+        Parameters
+        ----------
+        dst_id : str
+            ID of the destination node.
+        """
+
+        self._dst_id = dst_id
