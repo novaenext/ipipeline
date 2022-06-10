0e58591ff3f0d520b3efaad099402ed79a7ddf18
fnnmoro <fnnmoro@gmail.com>
2022-06-10 12:05:07 -0300

    test(TestPipeline): delete the _graph attr

--- tests/structure/test_pipeline.py
+++ tests/structure/test_pipeline.py
@@ -6,58 +6,41 @@ from ipipeline.structure.pipeline import Pipeline
 
 class TestPipeline(TestCase):
     def setUp(self) -> None:
-        self._mock_graph = {'n1': [], 'n2': []}
         self._mock_nodes = {'n1': None, 'n2': None}
         self._mock_links = {'l1': None}
         self._mock_node = object()
         self._mock_task = [lambda arg1, arg2: arg1 + arg2]
 
-    def test_init__graph_eq_dict__nodes_eq_dict__links_eq_dict(self) -> None:
+    def test_init__nodes_eq_dict__links_eq_dict(self) -> None:
         pipeline = Pipeline(
-            'p1', 
-            graph=self._mock_graph, 
-            nodes=self._mock_nodes, 
-            links=self._mock_links, 
-            tags=['t1']
+            'p1', nodes=self._mock_nodes, links=self._mock_links, tags=['t1']
         )
 
         self.assertEqual(pipeline._id, 'p1')
-        self.assertDictEqual(pipeline._graph, {'n1': [], 'n2': []})
         self.assertDictEqual(pipeline._nodes, {'n1': None, 'n2': None})
         self.assertDictEqual(pipeline._links, {'l1': None})
         self.assertListEqual(pipeline._tags, ['t1'])
 
-    def test_get__graph_eq_dict__nodes_eq_dict__links_eq_dict(self) -> None:
+    def test_get__nodes_eq_dict__links_eq_dict(self) -> None:
         pipeline = Pipeline(
-            'p1', 
-            graph=self._mock_graph, 
-            nodes=self._mock_nodes, 
-            links=self._mock_links, 
-            tags=['t1']
+            'p1', nodes=self._mock_nodes, links=self._mock_links, tags=['t1']
         )
 
         self.assertEqual(pipeline.id, 'p1')
-        self.assertDictEqual(pipeline.graph, {'n1': [], 'n2': []})
         self.assertDictEqual(pipeline.nodes, {'n1': None, 'n2': None})
         self.assertDictEqual(pipeline.links, {'l1': None})
         self.assertListEqual(pipeline.tags, ['t1'])
 
-    def test_set__graph_eq_dict__nodes_eq_dict__links_eq_dict(self) -> None:
+    def test_set__nodes_eq_dict__links_eq_dict(self) -> None:
         pipeline = Pipeline(
-            'p1', 
-            graph=self._mock_graph, 
-            nodes=self._mock_nodes, 
-            links=self._mock_links, 
-            tags=['t1']
+            'p1', nodes=self._mock_nodes, links=self._mock_links, tags=['t1']
         )
         pipeline.id = 'p2'
-        pipeline.graph = {'n3': []}
         pipeline.nodes = {'n3': None}
         pipeline.links = {'l2': None}
         pipeline.tags = ['t2']
 
         self.assertEqual(pipeline.id, 'p2')
-        self.assertDictEqual(pipeline.graph, {'n3': []})
         self.assertDictEqual(pipeline.nodes, {'n3': None})
         self.assertDictEqual(pipeline.links, {'l2': None})
         self.assertListEqual(pipeline.tags, ['t2'])
@@ -75,11 +58,7 @@ class TestPipeline(TestCase):
         self.assertFalse(checked)
 
     def test_add_node__id_eq_id(self) -> None:
-        pipeline = Pipeline(
-            'p1', 
-            graph=self._mock_graph, 
-            nodes=self._mock_nodes
-        )
+        pipeline = Pipeline('p1', nodes=self._mock_nodes)
 
         with self.assertRaisesRegex(
             PipelineError, r'id was found in the _nodes: id == n1'
@@ -100,8 +79,6 @@ class TestPipeline(TestCase):
             outputs=['sum']
         )
 
-        self.assertListEqual(list(pipeline.graph.keys()), ['n1'])
-        self.assertListEqual(list(pipeline.graph.values()), [[]])
         self.assertListEqual(list(pipeline.nodes.keys()), ['n1'])
         self.assertListEqual(
             [node.id for node in pipeline.nodes.values()], ['n1']
@@ -161,10 +138,7 @@ class TestPipeline(TestCase):
 
     def test_add_link__id_eq_id__src_id_eq_id__dst_id_eq_id(self) -> None:
         pipeline = Pipeline(
-            'p1', 
-            graph=self._mock_graph, 
-            nodes=self._mock_nodes, 
-            links=self._mock_links
+            'p1', nodes=self._mock_nodes, links=self._mock_links
         )
 
         with self.assertRaisesRegex(
@@ -173,26 +147,16 @@ class TestPipeline(TestCase):
             pipeline.add_link('l1', 'n1', 'n2')
 
     def test_add_link__id_ne_id__src_id_eq_id__dst_id_eq_id(self) -> None:
-        pipeline = Pipeline(
-            'p1', 
-            graph=self._mock_graph, 
-            nodes=self._mock_nodes
-        )
+        pipeline = Pipeline('p1',  nodes=self._mock_nodes)
         pipeline.add_link('l1', 'n1', 'n2')
 
-        self.assertListEqual(list(pipeline.graph.keys()), ['n1', 'n2'])
-        self.assertListEqual(list(pipeline.graph.values()), [['n2'], []])
         self.assertListEqual(list(pipeline.links.keys()), ['l1'])
         self.assertListEqual(
             [link.id for link in pipeline.links.values()], ['l1']
         )
 
     def test_add_link__id_ne_id__src_id_ne_id__dst_id_eq_id(self) -> None:
-        pipeline = Pipeline(
-            'p1', 
-            graph=self._mock_graph, 
-            nodes=self._mock_nodes
-        )
+        pipeline = Pipeline('p1', nodes=self._mock_nodes)
 
         with self.assertRaisesRegex(
             PipelineError, r'src_id was not found in the _nodes: src_id == n0'
@@ -200,11 +164,7 @@ class TestPipeline(TestCase):
             pipeline.add_link('l1', 'n0', 'n1')
 
     def test_add_link__id_ne_id__src_id_eq_id__dst_id_ne_id(self) -> None:
-        pipeline = Pipeline(
-            'p1', 
-            graph=self._mock_graph, 
-            nodes=self._mock_nodes
-        )
+        pipeline = Pipeline('p1', nodes=self._mock_nodes)
 
         with self.assertRaisesRegex(
             PipelineError, r'dst_id was not found in the _nodes: dst_id == n3'
