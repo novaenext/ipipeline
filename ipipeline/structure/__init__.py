5e3dfa1d271c8c7c7667263b5bbeb1f8443418c2
fnnmoro <fnnmoro@gmail.com>
2022-06-20 14:57:12 -0300

    refac(structure): delete the signal module

--- ipipeline/structure/__init__.py
+++ ipipeline/structure/__init__.py
@@ -5,4 +5,3 @@ The structure package provides components to store the tasks.
 
 from ipipeline.structure.catalog import Catalog
 from ipipeline.structure.pipeline import Pipeline
-from ipipeline.structure.signal import Signal
