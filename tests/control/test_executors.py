4b73eb3d19e6ef3104077f17047dfe84fca6ce76
fnnmoro <fnnmoro@gmail.com>
2022-06-30 12:08:39 -0300

    test(test_executors): update the tests according to new interface

--- tests/control/test_executors.py
+++ tests/control/test_executors.py
@@ -6,163 +6,116 @@ from ipipeline.structure.catalog import Catalog
 from ipipeline.structure.pipeline import Pipeline
 
 
-class MockBaseExecutor(BaseExecutor):
-    def execute_pipeline(self) -> None:
-        pass
-
-
 class TestBaseExecutor(TestCase):
     def setUp(self) -> None:
-        self._pipeline = Pipeline(
-            'p1', nodes=None, links=None, tags=['t1']
-        )
+        BaseExecutor.__abstractmethods__ = frozenset()
+
+        self._pipeline = Pipeline('p1', tags=['t1'])
         self._pipeline.add_node(
             'n1', 
-            mock_sum, 
-            inputs={'param1': 7, 'param2': 3}, 
-            outputs=['sum'], 
-            tags=None
-        )
-        self._pipeline.add_node(
-            'n2', 
-            mock_sub, 
-            inputs={'param1': 7, 'param2': 3}, 
-            outputs=['sub'], 
-            tags=None
-        )
-
-        self._catalog = Catalog('c1', items=None, tags=['t1'])
-        self._catalog.set_item('i1', 7)
-
-    def test_init(self) -> None:
-        executor = MockBaseExecutor(
-            pipeline=self._pipeline, catalog=self._catalog
-        )
-
-        self.assertDictEqual(executor.catalog.items, {'i1': 7})
-
-    def test_defaults(self) -> None:
-        executor1 = MockBaseExecutor()
-        executor2 = MockBaseExecutor()
-
-        self.assertIsNot(executor1.pipeline, executor2.pipeline)
-        self.assertIsNot(executor1.catalog, executor2.catalog)
-
-    def test_add_inexistent_pipeline(self) -> None:
-        executor = MockBaseExecutor(pipeline=None, catalog=None)
-        executor.add_pipeline(self._pipeline)
-
-        self.assertEqual(executor.pipeline.id, 'p1')
-        self.assertEqual(executor.pipeline.tags, ['t1'])
-
-    def test_add_default_pipeline(self) -> None:
-        executor = MockBaseExecutor(pipeline=None, catalog=None)
-        executor.add_pipeline(None)
-
-        self.assertEqual(executor.pipeline.id, 'p0')
-        self.assertEqual(executor.pipeline.tags, ['default'])
-
-    def test_add_inexistent_catalog(self) -> None:
-        executor = MockBaseExecutor(pipeline=None, catalog=None)
-        executor.add_catalog(self._catalog)
-
-        self.assertEqual(executor.catalog.id, 'c1')
-        self.assertEqual(executor.catalog.tags, ['t1'])
-
-    def test_add_default_catalog(self) -> None:
-        executor = MockBaseExecutor(pipeline=None, catalog=None)
-        executor.add_catalog(None)
-
-        self.assertEqual(executor.catalog.id, 'c0')
-        self.assertEqual(executor.catalog.tags, ['default'])
-
-    def test_execute_existent_node(self) -> None:
-        executor = MockBaseExecutor(
-            pipeline=self._pipeline, catalog=None
-        )
-        func_outputs = executor.execute_node('n1')
-
-        self.assertDictEqual(func_outputs, {'sum': 10})
-
-    def test_execute_inexistent_node(self) -> None:
-        executor = MockBaseExecutor(
-            pipeline=self._pipeline, catalog=None
-        )
-
-        with self.assertRaisesRegex(
-            ExecutorError, r'node not executed by the executor: id == n11'
-        ):
-            _ = executor.execute_node('n11')
-
-    def test_obtain_topo_order(self) -> None:
-        executor = MockBaseExecutor(
-            pipeline=self._pipeline, catalog=None
-        )
-        topo_order = executor.obtain_topo_order()
-
-        self.assertListEqual(topo_order, [['n1', 'n2']])
-
-
-class TestSequentialExecutor(TestCase):
-    def setUp(self) -> None:
-        self._pipeline = Pipeline(
-            'p1', nodes=None, links=None, tags=None
-        )
-        self._pipeline.add_node(
-            'n1', 
-            mock_sum, 
-            inputs={'param1': 7, 'param2': 3}, 
-            outputs=['sum'], 
+            lambda p1, p2: p1 + p2, 
+            pos_inputs=['i1', 'i2'], 
+            outputs=['i3'], 
             tags=['t1']
         )
         self._pipeline.add_node(
             'n2', 
-            mock_sub, 
-            inputs={'param1': 7, 'param2': 3}, 
-            outputs=['sub'], 
+            lambda p3=0: print(f'p3: {p3}'), 
+            key_inputs={'p3': 'i3'}, 
             tags=['t2']
         )
         self._pipeline.add_node(
             'n3', 
-            mock_print, 
-            inputs={'param1': 'c.sum', 'param2': '<-'}, 
-            outputs=None, 
+            lambda: [][0], 
+            tags=['t3']
+        )
+        self._pipeline.add_link('l1', 'n1', 'n2')
+        self._pipeline.add_link('l2', 'n1', 'n3')
+
+        self._catalog = Catalog('c1', tags=['t1'])
+        self._catalog.set_item('i1', 2)
+        self._catalog.set_item('i2', 4)
+
+    def test_get_ordering__pipeline_wi_nodes_wi_links(self) -> None:
+        executor = BaseExecutor()
+        ordering = executor.get_ordering(self._pipeline)
+
+        self.assertListEqual(ordering, [['n1'], ['n2', 'n3']])
+
+    def test_get_ordering__pipeline_wo_nodes_wo_links(self) -> None:
+        executor = BaseExecutor()
+        ordering = executor.get_ordering(Pipeline('p1', tags=['t1']))
+
+        self.assertListEqual(ordering, [])
+
+    def test_execute_node__node_wi_exception(self) -> None:
+        executor = BaseExecutor()
+
+        with self.assertRaisesRegex(
+            ExecutorError, r'node was not executed by the executor: id == n3'
+        ):
+            _ = executor.execute_node(self._pipeline, self._catalog, 'n3')
+
+    def test_execute_node__node_wo_exception(self) -> None:
+        executor = BaseExecutor()
+        items = executor.execute_node(self._pipeline, self._catalog, 'n1')
+
+        self.assertDictEqual(items, {'i3': 6})
+
+
+class TestSequentialExecutor(TestCase):
+    def setUp(self) -> None:
+        self._pipeline = Pipeline('p1', tags=['t1'])
+        self._pipeline.add_node(
+            'n1', 
+            lambda p1, p2: p1 + p2, 
+            pos_inputs=['i1', 'i2'], 
+            outputs=['i3'], 
+            tags=['t1']
+        )
+        self._pipeline.add_node(
+            'n2', 
+            lambda p1, p2: [p1 - p2, 0], 
+            pos_inputs=['i1', 'i2'], 
+            outputs=['i4', 'i5'], 
+            tags=['t2']
+        )
+        self._pipeline.add_node(
+            'n3', 
+            lambda p3=0: print(f'p3: {p3}'), 
+            key_inputs={'p3': 'i3'}, 
             tags=['t3']
         )
         self._pipeline.add_node(
             'n4', 
-            mock_print, 
-            inputs={'param1': 'c.sub'}, 
-            outputs=None, 
-            tags=None
-        )
-        self._pipeline.add_link('l1', 'n1', 'n3', tags=None)
-        self._pipeline.add_link('l2', 'n2', 'n4', tags=None)
-
-    def test_deriv(self) -> None:
-        executor = SequentialExecutor(
-            pipeline=None, catalog=None
+            lambda p4=0: print(f'p4: {p4}'), 
+            key_inputs={'p4': 'i4'}, 
+            tags=['t4']
         )
 
-        self.assertIsInstance(executor, BaseExecutor)
+        self._pipeline.add_link('l1', 'n1', 'n3')
+        self._pipeline.add_link('l2', 'n2', 'n4')
 
-    def test_execute_pipeline(self) -> None:
-        executor = SequentialExecutor(
-            pipeline=self._pipeline, catalog=None
+        self._catalog = Catalog('c1', tags=['t1'])
+        self._catalog.set_item('i1', 2)
+        self._catalog.set_item('i2', 4)
+
+        self._ordering = [['n1', 'n2'], ['n3', 'n4']]
+
+    def test_execute_pipeline__pipeline_wi_nodes_wi_links(self) -> None:
+        executor = SequentialExecutor()
+        catalog = executor.execute_pipeline(
+            self._pipeline, self._catalog, self._ordering
         )
-        topo_order = executor.obtain_topo_order()
-        executor.execute_pipeline(topo_order)
 
-        self.assertDictEqual(executor.catalog.items, {'sum': 10, 'sub': 4})
+        self.assertDictEqual(
+            catalog.items, {'i1': 2, 'i2': 4, 'i3': 6, 'i4': -2, 'i5': 0}
+        )
 
+    def test_execute_pipeline__pipeline_wo_nodes_wo_links(self) -> None:
+        executor = SequentialExecutor()
+        catalog = executor.execute_pipeline(
+            Pipeline('p1', tags=['t1']), Catalog('c1', tags=['t1']), []
+        )
 
-def mock_sum(param1: int, param2: int) -> int:
-    return param1 + param2
-
-
-def mock_sub(param1: int, param2: int) -> int:
-    return param1 - param2
-
-
-def mock_print(param1: int, param2: str = '->') -> None:
-    print(param1, param2)
+        self.assertDictEqual(catalog.items, {})
