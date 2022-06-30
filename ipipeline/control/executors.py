d45b2042793f06fd8c537e0f69555248a7853a2e
fnnmoro <fnnmoro@gmail.com>
2022-06-30 10:43:19 -0300

    refac(BaseExecutor): delete the get, set, and add methods

--- ipipeline/control/executors.py
+++ ipipeline/control/executors.py
@@ -11,102 +11,13 @@ from ipipeline.control.sorting import sort_topology
 from ipipeline.exceptions import ExecutorError
 from ipipeline.structure.catalog import Catalog
 from ipipeline.structure.pipeline import Pipeline
-from ipipeline.utils.checking import check_none
 
 
 logger = logging.getLogger(name=__name__)
 
 
 class BaseExecutor(ABC):
-    """Provides an interface to the executor classes.
-
-    The execution of the graph stored in the pipeline is according to its 
-    topological order. The returns from each executed node are stored in 
-    the catalog to provide access to subsequent nodes.
-
-    Attributes
-    ----------
-    _pipeline : Pipeline
-        Pipeline that stores a graph.
-    _catalog : Catalog
-        Catalog that stores the items from an execution.
-    """
-
-    def __init__(
-        self, pipeline: Pipeline = None, catalog: Catalog = None
-    ) -> None:
-        """Initializes the attributes.
-
-        Parameters
-        ----------
-        pipeline : Pipeline
-            Pipeline that stores a graph.
-        catalog : Catalog
-            Catalog that stores the items from an execution.
-        """
-
-        self.add_pipeline(pipeline)
-        self.add_catalog(catalog)
-
-    @property
-    def pipeline(self) -> Pipeline:
-        """Obtains the _pipeline attribute.
-
-        Returns
-        -------
-        pipeline : Pipeline
-            Pipeline that stores a graph.
-        """
-
-        return self._pipeline
-
-    @property
-    def catalog(self) -> Catalog:
-        """Obtains the _catalog attribute.
-
-        Returns
-        -------
-        catalog : Catalog
-            Catalog that stores the items from an execution.
-        """
-
-        return self._catalog
-
-    def add_pipeline(self, pipeline: Pipeline) -> None:
-        """Adds a pipeline in the executor.
-
-        Parameters
-        ----------
-        pipeline : Pipeline
-            Pipeline that stores a graph.
-
-        Raises
-        ------
-        InfoError
-            Informs that the id was not validated according to the pattern.
-        """
-
-        self._pipeline = check_none(
-            pipeline, Pipeline('p0', tags=['default'])
-        )
-
-    def add_catalog(self, catalog: Catalog) -> None:
-        """Adds a catalog in the executor.
-
-        Parameters
-        ----------
-        catalog : Catalog
-            Catalog that stores the items from an execution.
-
-        Raises
-        ------
-        InfoError
-            Informs that the id was not validated according to the pattern.
-        """
-
-        self._catalog = check_none(
-            catalog, Catalog('c0', tags=['default'])
-        )
+    """Provides an interface to the executor classes."""
 
     def get_ordering(self, pipeline: Pipeline) -> List[list]:
         """Gets the ordering of a graph.
@@ -205,19 +116,7 @@ class BaseExecutor(ABC):
 
 
 class SequentialExecutor(BaseExecutor):
-    """Executes a pipeline sequentially.
-
-    The execution of the graph stored in the pipeline is according to its 
-    topological order. The returns from each executed node are stored in 
-    the catalog to provide access to subsequent nodes.
-
-    Attributes
-    ----------
-    _pipeline : Pipeline
-        Pipeline that stores a graph.
-    _catalog : Catalog
-        Catalog that stores the items from an execution.
-    """
+    """Executes a pipeline sequentially."""
 
     def execute_pipeline(
         self, pipeline: Pipeline, catalog: Catalog, ordering: List[list]
@@ -245,6 +144,10 @@ class SequentialExecutor(BaseExecutor):
         ExecutorError
             Informs that the node was not executed by the executor.
         """
+
+        logger.info(
+            f'pipeline.id: {pipeline.id}, pipeline.tags: {pipeline.tags}'
+        )
 
         for group in ordering:
             for node_id in group:
