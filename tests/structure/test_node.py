0df3dd2dff806202fda4fbfa9b3202c3f1dcfd52
fnnmoro <fnnmoro@gmail.com>
2022-10-24 07:50:08 -0300

    test(TestNode): add the test_build_items* methods

--- tests/structure/test_node.py
+++ tests/structure/test_node.py
@@ -68,77 +68,141 @@ class TestNode(TestCase):
         self.assertListEqual(node.tags, ['t2'])
 
     def test_build_pos_args__pos_inputs_wi_ids(self) -> None:
-        node = Node(
-            'n1', 
-            self._tasks[0], 
-            pos_inputs=['i1', 'i2'], 
-            outputs=['o1'], 
-            tags=['t1']
-        )
+        node = Node('n1', self._tasks[0], pos_inputs=['i1', 'i2'])
         pos_args = node.build_pos_args(self._catalog)
 
         self.assertListEqual(pos_args, [2, 4])
 
     def test_build_pos_args__pos_inputs_wo_ids(self) -> None:
-        node = Node(
-            'n1', 
-            self._tasks[0], 
-            pos_inputs=[], 
-            outputs=['o1'], 
-            tags=['t1']
-        )
+        node = Node('n1', self._tasks[0], pos_inputs=[])
         pos_args = node.build_pos_args(self._catalog)
 
         self.assertListEqual(pos_args, [])
 
     def test_build_key_args__key_inputs_wi_ids(self) -> None:
-        node = Node(
-            'n1', 
-            self._tasks[0], 
-            key_inputs={'p1': 'i1', 'p2': 'i2'}, 
-            outputs=['o1'], 
-            tags=['t1']
-        )
+        node = Node('n1', self._tasks[0], key_inputs={'p1': 'i1', 'p2': 'i2'})
         key_args = node.build_key_args(self._catalog)
 
         self.assertDictEqual(key_args, {'p1': 2, 'p2': 4})
 
     def test_build_key_args__key_inputs_wo_ids(self) -> None:
-        node = Node(
-            'n1', 
-            self._tasks[0], 
-            key_inputs={}, 
-            outputs=['o1'], 
-            tags=['t1']
-        )
+        node = Node('n1', self._tasks[0], key_inputs={})
         key_args = node.build_key_args(self._catalog)
 
         self.assertDictEqual(key_args, {})
 
     def test_execute_task__args_wi_values(self) -> None:
-        node = Node(
-            'n1', 
-            self._tasks[0], 
-            pos_inputs=['i1'], 
-            key_inputs={'p2': 'i2'}, 
-            outputs=['o1'], 
-            tags=['t1']
-        )
+        node = Node('n1', self._tasks[0])
         results = node.execute_task([2], {'p2': 4})
 
         self.assertListEqual(results, [2, 4])
 
     def test_execute_task__args_wo_values(self) -> None:
-        node = Node(
-            'n1', 
-            self._tasks[0], 
-            pos_inputs=['i1'], 
-            key_inputs={'p2': 'i2'}, 
-            outputs=['o1'], 
-            tags=['t1']
-        )
+        node = Node('n1', self._tasks[0])
 
         with self.assertRaisesRegex(
             NodeError, r'task was not executed: id == n1'
         ):
             _ = node.execute_task([], {})
+
+    def test_build_items__outputs_wi_one__results_wi_one(self) -> None:
+        node = Node('n1', None, outputs=['o1'])
+        items = node.build_items(['r1'])
+
+        self.assertDictEqual(items, {'o1': ['r1']})
+
+    def test_build_items__outputs_wi_one__results_wi_zero(self) -> None:
+        node = Node('n1', None, outputs=['o1'])
+        items = node.build_items([])
+
+        self.assertDictEqual(items, {'o1': []})
+
+    def test_build_items__outputs_wi_zero__results_wi_one(self) -> None:
+        node = Node('n1', None, outputs=[])
+        items = node.build_items(['r1'])
+
+        self.assertDictEqual(items, {})
+
+    def test_build_items__outputs_wi_zero__results_wi_zero(self) -> None:
+        node = Node('n1', None, outputs=[])
+        items = node.build_items([])
+
+        self.assertDictEqual(items, {})
+
+    def test_build_items__outputs_wi_two__results_wi_two(self) -> None:
+        node = Node('n1', None, outputs=['o1', 'o2'])
+        items = node.build_items(['r1', 'r2'])
+
+        self.assertDictEqual(items, {'o1': 'r1', 'o2': 'r2'})
+
+    def test_build_items__outputs_wi_two__results_wi_zero(self) -> None:
+        node = Node('n1', None, outputs=['o1', 'o2'])
+
+        with self.assertRaisesRegex(
+            NodeError, 
+            r'outputs did not match the results in terms of size: 2 != 0'
+        ):
+            _ = node.build_items([])
+
+    def test_build_items__outputs_wi_two__results_wi_one(self) -> None:
+        node = Node('n1', None, outputs=['o1', 'o2'])
+
+        with self.assertRaisesRegex(
+            NodeError, 
+            r'outputs did not match the results in terms of size: 2 != 1'
+        ):
+            _ = node.build_items(['r1'])
+
+    def test_build_items__outputs_wi_one__results_wi_two(self) -> None:
+        node = Node('n1', None, outputs=['o1'])
+        items = node.build_items(['r1', 'r2'])
+
+        self.assertDictEqual(items, {'o1': ['r1', 'r2']})
+
+    def test_build_items__outputs_wi_three__results_wi_three(self) -> None:
+        node = Node('n1', None, outputs=['o1', 'o2', 'o3'])
+        items =node.build_items(['r1', 'r2', 'r3'])
+
+        self.assertDictEqual(items, {'o1': 'r1', 'o2': 'r2', 'o3': 'r3'})
+
+    def test_build_items__outputs_wi_three__results_wi_two(self) -> None:
+        node = Node('n1', None, outputs=['o1', 'o2', 'o3'])
+
+        with self.assertRaisesRegex(
+            NodeError, 
+            r'outputs did not match the results in terms of size: 3 != 2'
+        ):
+            _ = node.build_items(['r1', 'r2'])
+
+    def test_build_items__outputs_wi_two__results_wi_three(self) -> None:
+        node = Node('n1', None, outputs=['o1', 'o2'])
+
+        with self.assertRaisesRegex(
+            NodeError, 
+            r'outputs did not match the results in terms of size: 2 != 3'
+        ):
+            _ = node.build_items(['r1', 'r2', 'r3'])
+
+    def test_build_items__outputs_wi_none__results_wo_none(self) -> None:
+        node = Node('n1', None, outputs=None)
+        items = node.build_items('r1')
+
+        self.assertDictEqual(items, {})
+
+    def test_build_items__outputs_wo_none__results_wi_none(self) -> None:
+        node = Node('n1', None, outputs=['o1'])
+        items = node.build_items(None)
+
+        self.assertDictEqual(items, {'o1': None})
+
+    def test_build_items__outputs_wi_none__results_wi_none(self) -> None:
+        node = Node('n1', None, outputs=None)
+        items = node.build_items(None)
+
+        self.assertDictEqual(items, {})
+
+    def test_build_items__outputs_wo_none__results_wo_none(self) -> None:
+        node = Node('n1', None, outputs=['o1'])
+        items = node.build_items('r1')
+
+        self.assertDictEqual(items, {'o1': 'r1'})
